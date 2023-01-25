8c4cec68499243823d43d6ff4bc389c17d9f8ec8
hardyyb2 <hardikb3732@gmail.com>
2023-01-25 22:19:12 +0530

    [ADDED] vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
@@ -5,4 +6,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), eslint()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
 });
