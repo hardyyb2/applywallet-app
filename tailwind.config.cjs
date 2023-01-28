3bcc6aa3cf466ce81d8ebc20fa7b5a6256339011
Hardik_B <hardik.badola@yellow.ai>
2023-01-28 19:09:34 +0530

    [UPDATED] tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark"],
