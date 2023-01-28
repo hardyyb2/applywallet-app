4612259954126249384bcc743af62f109ab1a1d8
Hardik_B <hardik.badola@yellow.ai>
2023-01-28 19:20:23 +0530

    [ADDED] Input

---
+++ src/components/wrapped/Input/Input.tsx
@@ -0,0 +1,47 @@
+import React, { forwardRef } from "react";
+import clsx from "clsx";
+
+import { InputColorsType, InputSizeTypes } from "./Input.types";
+
+export type InputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, "size" | "color"> & {
+  bordered?: boolean;
+  borderOffset?: boolean;
+  size?: InputSizeTypes;
+  color?: InputColorsType;
+  htmlSize?: number;
+};
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ bordered = true, borderOffset = true, size, color, className, htmlSize, ...props }, ref): JSX.Element => {
+    const classes = clsx(
+      "input",
+      {
+        "input-lg": size === "lg",
+        "input-md": size === "md",
+        "input-sm": size === "sm",
+        "input-xs": size === "xs",
+      },
+      {
+        "input-primary": color === "primary",
+        "input-secondary": color === "secondary",
+        "input-accent": color === "accent",
+        "input-info": color === "info",
+        "input-success": color === "success",
+        "input-warning": color === "warning",
+        "input-error": color === "error",
+        "input-ghost": color === "ghost",
+      },
+      {
+        "focus:outline-offset-0": !borderOffset,
+        "input-bordered": bordered,
+      },
+      className,
+    );
+
+    return <input ref={ref} size={htmlSize} className={classes} {...props} />;
+  },
+);
+
+Input.displayName = "Input";
+
+export default Input;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,13 @@
 import { FC } from "react";
 
 import { Flex } from "@/components/common/Flex";
-import { Button } from "@/components/wrapped";
+import { Button, Input } from "@/components/wrapped";
 
 const Home: FC = () => {
   return (
     <Flex className="gap-2">
-      {/* <Input color="secondary" /> */}
+      <Input color="secondary" />
       <Button color="secondary"> Hello </Button>
-      {/* <button className="btn btn-primary">Hello</button> */}
     </Flex>
   );
 };
