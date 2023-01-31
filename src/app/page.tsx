da42cf93236fccdfa983c2ec699556089fc9614d
Hardik_B <hardik.badola@yellow.ai>
2023-01-31 19:51:41 +0530

    [ADDED] Eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import { Inter } from "@next/font/google";
+import Image from "next/image";
+
 import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,13 +21,27 @@ export default function Home() {
             className="bg-red-500"
           >
             By{" "}
-            <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
         <div className={styles.thirteen}>
           <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
         </div>
@@ -42,7 +57,9 @@ export default function Home() {
           <h2 className={inter.className}>
             Docs <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>Find in-depth information about Next.js features and API.</p>
+          <p className={inter.className}>
+            Find in-depth information about Next.js features and API.
+          </p>
         </a>
 
         <a
@@ -66,7 +83,9 @@ export default function Home() {
           <h2 className={inter.className}>
             Deploy <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+          <p className={inter.className}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
         </a>
       </div>
     </main>
