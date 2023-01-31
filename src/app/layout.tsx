903423f9fa5627c14eade59b3c3c708226170378
Hardik_B <hardik.badola@yellow.ai>
2023-01-31 20:55:21 +0530

    [ADDED] Base Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { BaseLayout } from "@/components/dependent/page_components/common";
+import { Drawer, Navbar } from "@/components/isolated/wrapped";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,12 +9,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body className="bg-red-700">{children}</body>
+      <body>
+        <BaseLayout className="h-full">
+          <BaseLayout.Head>
+            <Navbar className="bg-primary text-primary-content">
+              <Navbar.Center>hardik badola</Navbar.Center>
+            </Navbar>
+          </BaseLayout.Head>
+          <BaseLayout.Body className="overflow-hidden">
+            <Drawer mobile className="h-full">
+              <Drawer.Side>
+                <ul className="menu w80 p-4">
+                  {Array(900)
+                    .fill(undefined)
+                    .map((_item, index) => (
+                      <li key={index} className="">
+                        <a href="#a">{index}</a>
+                      </li>
+                    ))}
+                </ul>
+                {/* <ScrollArea.Root>
+                  <ScrollArea.Viewport className="w-full h-full">
+
+                  </ScrollArea.Viewport>
+                </ScrollArea.Root> */}
+              </Drawer.Side>
+              <Drawer.Main>{children}</Drawer.Main>
+            </Drawer>
+          </BaseLayout.Body>
+        </BaseLayout>
+      </body>
     </html>
   );
 }
