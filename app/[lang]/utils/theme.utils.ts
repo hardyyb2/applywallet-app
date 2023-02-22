a5b4fae5d7eee3eb8aea728e9130ca258789218b
Hardik_B <hardik.badola@yellow.ai>
2023-02-22 21:11:47 +0530

    [UPDATED] get theme to enable custom themes

--- app/[lang]/utils/theme.utils.ts
+++ app/[lang]/utils/theme.utils.ts
@@ -1,3 +1,17 @@
 import { tailwindConfig } from "./tailwind.utils";
 
-export const appThemes: string[] = tailwindConfig?.daisyui?.themes ?? ["light"];
+const getThemeNames = (themes: (string | Record<string, object>)[] = []) => {
+  return themes?.map((theme) => {
+    if (typeof theme === "object") {
+      const themeName = Object.keys(theme ?? {})?.[0];
+
+      return themeName;
+    }
+
+    return theme;
+  });
+};
+
+export const appThemes: string[] = getThemeNames(
+  tailwindConfig?.daisyui?.themes,
+) ?? ["light"];
