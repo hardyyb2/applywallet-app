187c9090b2e958e004b20c3f3786657ee61ab074
Hardik_B <hardik.badola@yellow.ai>
2023-03-05 16:39:43 +0530

    [ADDED] default motion-config

--- app/[lang]/providers.tsx
+++ app/[lang]/providers.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { LazyMotion, domAnimation } from "framer-motion";
+import { LazyMotion, domAnimation, MotionConfig } from "framer-motion";
 import { ReactNode } from "react";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <LazyMotion features={domAnimation}>{children}</LazyMotion>;
+  return (
+    <LazyMotion features={domAnimation}>
+      <MotionConfig
+        transition={{ type: "spring", stiffness: 400, damping: 24 }}
+      >
+        {children}
+      </MotionConfig>
+    </LazyMotion>
+  );
 };
 
 export { Providers };
