ad1e6779d3768c8a9ded5e1ec7f46abd43c96a09
Hardik_B <hardik.badola@yellow.ai>
2023-03-05 16:51:33 +0530

    [ADDING] reorder

--- app/[lang]/careers/components/CareerSettings/CareerSettingsContent.tsx
+++ app/[lang]/careers/components/CareerSettings/CareerSettingsContent.tsx
@@ -1,6 +1,20 @@
+"use client";
+
+import { Reorder } from "framer-motion";
+import { useState } from "react";
+
 const CareerSettingsContent = () => {
+  const [items, setItems] = useState([0, 1, 2, 3]);
+
   return (
     <section className="prose">
+      <Reorder.Group axis="y" values={items} onReorder={setItems}>
+        {items.map((item) => (
+          <Reorder.Item key={item} value={item}>
+            {item}
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
       <h2>rearrange information order</h2>
     </section>
   );
