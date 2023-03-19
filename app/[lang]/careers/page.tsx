dfdefdaf5f1aaa95152bba3cf12bfd57afefef79
Hardik_B <hardik.badola@yellow.ai>
2023-03-19 21:05:17 +0530

    [ADDED] mock folder

--- app/[lang]/careers/page.tsx
+++ app/[lang]/careers/page.tsx
@@ -9,90 +9,16 @@ import { AddIcon } from "public/images";
 import { CareerType } from "./career.types";
 import { CareerActions } from "./components/CareerActions/CareerActions";
 import { CareerCard } from "./components/CareerCard";
+import { careerDataMock } from "./mock/careers";
 
 export const metadata: Metadata = {
   title: "careers",
 };
 
-const data: CareerType[] = [
-  {
-    company: {
-      name: "ABC Corp",
-      description: "A multinational corporation",
-    },
-    role: {
-      name: "Marketing Manager",
-      description: "Responsible for developing marketing strategies",
-    },
-    job: {
-      description:
-        "Develop and execute marketing campaigns to drive brand awareness and customer acquisition",
-    },
-  },
-  {
-    company: {
-      name: "XYZ Inc",
-      description: "A startup company",
-    },
-    role: {
-      name: "Software Developer",
-      description:
-        "Responsible for developing and maintaining software applications",
-    },
-    job: {
-      description:
-        "Design and develop software applications using programming languages such as Java and Python",
-    },
-  },
-  {
-    company: {
-      name: "PQR Corp",
-      description: "A manufacturing company",
-    },
-    role: {
-      name: "Production Manager",
-      description: "Responsible for overseeing production processes",
-    },
-    job: {
-      description:
-        "Ensure that production processes are efficient and meet quality standards",
-    },
-  },
-  {
-    company: {
-      name: "LMN Inc",
-      description: "A consulting firm",
-    },
-    role: {
-      name: "Business Analyst",
-      description:
-        "Responsible for analyzing business data and providing insights",
-    },
-    job: {
-      description:
-        "Analyze business data and provide recommendations for improving business performance",
-    },
-  },
-  {
-    company: {
-      name: "EFG Corp",
-      description: "A technology company",
-    },
-    role: {
-      name: "Product Manager",
-      description: "Responsible for managing product development",
-    },
-    job: {
-      description:
-        "Manage product development projects and ensure that products meet customer needs and are delivered on time",
-    },
-  },
-];
-
 const fetchCareers = (): Promise<CareerType[]> => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(data);
+      resolve(careerDataMock);
     }, 2000);
   });
 };
