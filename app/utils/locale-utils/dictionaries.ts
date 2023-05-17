f8e2aeeba21b2bfa6b6e6dd44d649a02a448606b
hardik badola <hardikb3732@gmail.com>
2023-05-17 22:17:19 +0530

    [MOVED] dictionaries to public

--- app/utils/locale-utils/dictionaries.ts
+++ app/utils/locale-utils/dictionaries.ts
@@ -4,8 +4,10 @@ import type { Locale } from "app/utils/locale-utils/i18n-config";
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
 const dictionaries: Record<Locale, () => any> = {
-  en: () => import("dictionaries/en.json").then((module) => module.default),
-  es: () => import("dictionaries/es.json").then((module) => module.default),
+  en: () =>
+    import("public/dictionaries/en.json").then((module) => module.default),
+  es: () =>
+    import("public/dictionaries/es.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]?.();
