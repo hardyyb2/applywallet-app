e39b11e0542229874481fbc50011b7765bbcbfbe
hardik badola <hardikb3732@gmail.com>
2023-09-26 19:20:44 +0530

    [ADDED] interview POST API

---
+++ app/api/interviews/route.ts
@@ -0,0 +1,71 @@
+import { NextResponse } from "next/server";
+
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+import { authOptions } from "@/lib/auth";
+import { ApiError, ApiErrorCodes, ApiResponse } from "@/utils/api";
+import { CustomError } from "@/utils/error";
+import { interviewSchema } from "@/utils/schema-utils";
+import { SheetNames } from "@/utils/sheet.utils";
+import { zodKeys } from "@/utils/zod.utils";
+
+export async function POST(request: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.accessToken || !session.user?.primarySheetId) {
+      return NextResponse.json(
+        new ApiError({
+          code: ApiErrorCodes.UNAUTHORIZED,
+        }),
+        { status: 401 },
+      );
+    }
+
+    const json = await request.json();
+    const body = interviewSchema.parse(json);
+
+    const doc = new GoogleSpreadsheet(session.user.primarySheetId, {
+      token: session.accessToken,
+    });
+    await doc.loadInfo();
+
+    const allSheets = doc.sheetsByTitle;
+    let interviewSheet = allSheets[SheetNames.INTERVIEWS];
+    if (!interviewSheet) {
+      interviewSheet = await doc.addSheet({
+        title: SheetNames.INTERVIEWS,
+        headerValues: zodKeys(interviewSchema),
+      });
+    }
+    await interviewSheet.addRow(body);
+
+    return NextResponse.json(new ApiResponse(), {
+      status: 200,
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        new ApiError({
+          code: ApiErrorCodes.BAD_REQUEST,
+          data: error.issues,
+        }),
+        { status: 422 },
+      );
+    }
+
+    const message = new CustomError(error).message;
+
+    return NextResponse.json(
+      new ApiError({
+        code: ApiErrorCodes.SERVER_ERROR,
+        message,
+      }),
+      {
+        status: 500,
+      },
+    );
+  }
+}
