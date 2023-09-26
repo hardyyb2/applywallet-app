ac95ea5759525f3961a4e3f77fd107ba2f068b16
hardik badola <hardikb3732@gmail.com>
2023-09-26 19:20:58 +0530

    [ADDED] interview page

--- app/[lang]/(nav-layout)/(swr)/interviews/page.tsx
+++ app/[lang]/(nav-layout)/(swr)/interviews/page.tsx
@@ -1,18 +1,59 @@
-import { cn } from "@/utils/styles.utils";
+import type { Metadata } from "next";
+import Link from "next/link";
+
+import { getServerSession } from "next-auth";
+
+import { LoginCard } from "@/components/ui/dependent/LoginCard";
+import { Flex, Typography } from "@/components/ui/isolated/common";
+import { Button } from "@/components/ui/isolated/wrapped";
+import { Icons } from "@/components/ui/isolated/wrapped/Icons";
+import { authOptions } from "@/lib/auth";
 
 import { InterviewsList } from "./InterviewsList";
 
-const Interviews = () => {
+export const metadata: Metadata = {
+  title: "interviews",
+};
+
+const Interviews = async () => {
+  // constants
+  const session = await getServerSession(authOptions);
+  const isLoggedIn = session && session.accessToken;
+
+  if (!isLoggedIn) {
+    return (
+      <Flex justify="center" className="p-s">
+        <LoginCard />
+      </Flex>
+    );
+  }
+
   return (
-    <div className="p-8">
-      <div
-        className={cn(
-          "grid w-full gap-8",
-          "grid-cols-[repeat(auto-fill,minmax(256px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(320px,1fr))]",
-        )}
-      >
-        <InterviewsList />
-      </div>
+    <div className="px-6 py-4 lg:px-10">
+      <Flex justify="space-between" align="center" className="mb-m-l gap-4">
+        <Flex direction="column">
+          <Typography variant="h3">interviews</Typography>
+          <Typography variant="subtitle2">
+            your ongoing or past interviews
+          </Typography>
+        </Flex>
+
+        {/* <ExperienceActions /> */}
+      </Flex>
+
+      <InterviewsList />
+
+      <Link href="/interviews/add">
+        <Button
+          color="secondary"
+          fullWidth
+          size="lg"
+          className="mt-l lg:hidden"
+          startIcon={<Icons.Plus />}
+        >
+          add interview
+        </Button>
+      </Link>
     </div>
   );
 };
