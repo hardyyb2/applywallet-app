806a2d85901547f09227e64ae5e7d5b1d1b5f00e
hardik badola <hardikb3732@gmail.com>
2023-09-29 19:44:03 +0530

    [ADDED] fetch request in edit interview page

---
+++ app/[lang]/(nav-layout)/(swr)/interviews/edit/[interviewId]/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const error = () => {
+  return <div>Haan bhai error ho gaya</div>;
+};
+
+export default error;
---
+++ app/[lang]/(nav-layout)/(swr)/interviews/edit/[interviewId]/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+
+import { ConditionalMatch } from "@dx-kit/react-conditional-match";
+import { useInterview } from "queries/interviews.queries";
+
+import { Breadcrumbs } from "@/components/ui/isolated/wrapped";
+import { AppRoutes, UrlParams } from "@/utils/routes.utils";
+
+import { AddEditInterviewForm } from "../../components/AddEditInterviewForm";
+
+type EditInterviewProps = {
+  params: Record<string, string>;
+};
+
+const EditInterview = ({ params }: EditInterviewProps) => {
+  const interviewId = params[UrlParams.INTERVIEW_ID];
+
+  // TODO - get type here
+  const { data, isLoading, error } = useInterview(interviewId);
+
+  if (error) {
+    throw new Error();
+  }
+
+  return (
+    <div className="m-xs mt-0">
+      <Breadcrumbs className="mb-2xs pt-0 [&_a]:no-underline">
+        <Breadcrumbs.Item>
+          <Link href={AppRoutes.INTERVIEWS}>interviews</Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item>edit</Breadcrumbs.Item>
+      </Breadcrumbs>
+      <ConditionalMatch fallback="loading....">
+        <ConditionalMatch.Render when={!isLoading}>
+          <AddEditInterviewForm type="edit" interview={data} />
+        </ConditionalMatch.Render>
+      </ConditionalMatch>
+    </div>
+  );
+};
+
+export default EditInterview;
