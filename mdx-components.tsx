df0eaee401dcbca6a47f8cfb884c7d12e9ed4084
hardik badola <hardikb3732@gmail.com>
2023-09-30 14:21:33 +0530

    [ADDED] Typography component to mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,17 +1,9 @@
-import { cn } from "@/_utils/styles.utils";
+import { Typography } from "@/_components/ui/isolated/common/Typography/Typography";
 import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    h1: ({ className, ...props }) => (
-      <h1
-        className={cn(
-          "!text-4xl !mt-2 !scroll-m-20 !font-bold !tracking-tight",
-          className,
-        )}
-        {...props}
-      />
-    ),
+    Typography,
   };
 }
