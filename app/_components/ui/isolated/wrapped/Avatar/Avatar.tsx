179b3969809eb3d6d8f7ac202a249cc040ac795c
hardik badola <hardikb3732@gmail.com>
2023-10-06 14:44:48 +0530

    [UPDATED] Avatar component

--- app/_components/ui/isolated/wrapped/Avatar/Avatar.tsx
+++ app/_components/ui/isolated/wrapped/Avatar/Avatar.tsx
@@ -1,5 +1,53 @@
 "use client";
 
-import * as Avatar from "@radix-ui/react-avatar";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 
-export { Avatar };
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+
+import { cnMerge } from "@/utils/styles.utils";
+
+const Avatar = forwardRef<
+  ElementRef<typeof AvatarPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  // TODO - add various sizes
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cnMerge("avatar rounded-xl", className)}
+    {...props}
+  />
+));
+
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = forwardRef<
+  ElementRef<typeof AvatarPrimitive.Image>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+  // TODO - try to use next/image here or pass custom component for render
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cnMerge("aspect-square h-full w-full", className)}
+    {...props}
+  />
+));
+
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = forwardRef<
+  ElementRef<typeof AvatarPrimitive.Fallback>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cnMerge(
+      "flex h-full w-full items-center justify-center rounded-full",
+      className,
+    )}
+    {...props}
+  />
+));
+
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+export { Avatar, AvatarImage, AvatarFallback };
