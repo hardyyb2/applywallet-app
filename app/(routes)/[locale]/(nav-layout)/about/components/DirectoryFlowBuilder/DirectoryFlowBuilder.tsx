8f17e8907d757a3251cb5a5b88a320f36b4f1dca
hardik badola <hardikb3732@gmail.com>
2023-10-13 19:37:01 +0530

    [ADDED] basic collapse nodes

--- app/(routes)/[locale]/(nav-layout)/about/components/DirectoryFlowBuilder/DirectoryFlowBuilder.tsx
+++ app/(routes)/[locale]/(nav-layout)/about/components/DirectoryFlowBuilder/DirectoryFlowBuilder.tsx
@@ -2,6 +2,11 @@
 
 import {
   Background,
+  Edge,
+  getConnectedEdges,
+  getOutgoers,
+  Node,
+  NodeMouseHandler,
   NodeTypes,
   ReactFlow,
   useEdgesState,
@@ -18,7 +23,7 @@ import { getLayoutedElements } from "./directoryFlowBuilder.utils";
 
 import "reactflow/dist/style.css";
 
-import { useThemeColors } from "@/hooks/useThemeColors";
+import { useState } from "react";
 
 const nodeTypes: NodeTypes = {
   custom: CustomNode,
@@ -30,9 +35,75 @@ const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
 );
 
 const DirectoryFlowBuilder = () => {
-  const themeColors = useThemeColors();
-  const [nodes, _setNodes, onNodesChange] = useNodesState(layoutedNodes);
-  const [edges, _setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+  const [hidden, setHidden] = useState(true);
+
+  const hideNodes =
+    (hidden: boolean, childEdgesID: string[], childNodesID: string[]) =>
+    (node: Node) => {
+      if (childNodesID.includes(node.id)) {
+        node.hidden = hidden;
+      }
+
+      return node;
+    };
+
+  const hideEdges =
+    (hidden: boolean, childEdgesID: string[], childNodesID: string[]) =>
+    (edge: Edge) => {
+      if (childEdgesID.includes(edge.id) || childNodesID.includes(edge.id)) {
+        edge.hidden = hidden;
+      }
+
+      return edge;
+    };
+
+  const checkTarget = (edges: Edge[], id: string) => {
+    let egs = edges.filter((ed) => {
+      return ed.target !== id;
+    });
+    return egs;
+  };
+
+  const handleNodeClick: NodeMouseHandler = (_event, node) => {
+    let outgoers: Node[] = [];
+    let connectedEdges: Edge[] = [];
+    let stack: Node[] = [];
+
+    let currentNodeID = node.id;
+
+    stack.push(node);
+    while (stack.length > 0) {
+      let lastNode = stack.pop();
+
+      if (lastNode) {
+        let childnode = getOutgoers(lastNode, nodes, edges);
+        let childedge = checkTarget(
+          getConnectedEdges([lastNode], edges),
+          currentNodeID,
+        );
+        childnode.map((goer) => {
+          stack.push(goer);
+          outgoers.push(goer);
+        });
+        childedge.map((edge) => {
+          connectedEdges.push(edge);
+        });
+      }
+    }
+
+    let childNodeID = outgoers.map((node) => {
+      return node.id;
+    });
+    let childEdgeID = connectedEdges.map((edge) => {
+      return edge.id;
+    });
+
+    setNodes((nodes) => nodes.map(hideNodes(hidden, childEdgeID, childNodeID)));
+    setEdges((edges) => edges.map(hideEdges(hidden, childEdgeID, childNodeID)));
+    setHidden(!hidden);
+  };
 
   return (
     <ReactFlow
@@ -47,6 +118,7 @@ const DirectoryFlowBuilder = () => {
       zoomOnPinch
       zoomOnDoubleClick
       maxZoom={4}
+      onNodeClick={handleNodeClick}
     >
       <Background gap={16} />
     </ReactFlow>
