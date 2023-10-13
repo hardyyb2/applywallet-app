083de72535400c9b1e6e044f53d061b1c81b3659
hardik badola <hardikb3732@gmail.com>
2023-10-13 19:02:40 +0530

    [ADDED] useThemeColors hook

---
+++ app/_hooks/useThemeColors.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { extractThemeColorsFromDOM, ThemeColors } from "@/utils/theme.utils";
+
+const useThemeColors = () => {
+  const [themeColors, setThemeColors] = useState<ThemeColors | null>(null);
+
+  useEffect(() => {
+    setThemeColors(extractThemeColorsFromDOM());
+  }, []);
+
+  return themeColors;
+};
+
+export { useThemeColors };
