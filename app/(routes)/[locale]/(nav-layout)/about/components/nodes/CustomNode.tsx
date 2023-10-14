002b90eba8ac669b95b4313c41a64b0f0f3faf4c
hardik badola <hardikb3732@gmail.com>
2023-10-14 14:53:22 +0530

    [ADDED] collapse icon to custom nodes

--- app/(routes)/[locale]/(nav-layout)/about/components/nodes/CustomNode.tsx
+++ app/(routes)/[locale]/(nav-layout)/about/components/nodes/CustomNode.tsx
@@ -1,8 +1,16 @@
 import { Fragment } from "react";
 
+import { ConditionalMatch } from "@dx-kit/react-conditional-match";
 import { Handle, NodeProps, Position } from "reactflow";
 
 import { Flex, Typography } from "@/components/ui/isolated/common";
+import {
+  Button,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/isolated/wrapped";
 import { Icons } from "@/components/ui/isolated/wrapped/Icons";
 
 import { DrNodeDataType } from "@/types/flowbuilder";
@@ -12,6 +20,7 @@ import { customNodeIcon } from "./customNode.utils";
 // TODO - check why memo is not working properly
 const CustomNode = ({ data, isConnectable }: NodeProps<DrNodeDataType>) => {
   const NodeIcon = customNodeIcon[`${data.type}:${data.subType}`];
+  const hasNodeIcon = data.type !== "file";
 
   return (
     <Fragment>
@@ -28,7 +37,27 @@ const CustomNode = ({ data, isConnectable }: NodeProps<DrNodeDataType>) => {
         <Typography variant="body2" className="!font-bold text-base-content">
           {data.name}
         </Typography>
-        {data.collapsed ? <Icons.ChevronDown /> : <Icons.ChevronsRight />}
+
+        <ConditionalMatch fallback={null}>
+          <ConditionalMatch.Render when={hasNodeIcon}>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button color="ghost" size="sm">
+                    {data.collapsed ? (
+                      <Icons.ChevronDown />
+                    ) : (
+                      <Icons.ChevronsRight />
+                    )}
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  {data.collapsed ? "expand children" : "collapse children"}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </ConditionalMatch.Render>
+        </ConditionalMatch>
       </Flex>
 
       <Handle
