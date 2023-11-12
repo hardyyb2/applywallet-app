88ebc759bc18fc1e3fb46a1689701a2c3d3330bb
hardik badola <hardikb3732@gmail.com>
2023-11-12 21:04:25 +0530

    [ADDED] extApiTyped

--- src/lib/extApi.ts
+++ src/lib/extApi.ts
@@ -1,8 +1,21 @@
-import axios from "axios";
+import axios, { type AxiosRequestConfig } from "axios";
+import type { z } from "zod";
 
 const extApi = axios.create({
   baseURL: process.env.PLASMO_PUBLIC_API_URL,
   withCredentials: true,
 });
 
-export { extApi };
+const extApiTyped = async <T>(
+  config: AxiosRequestConfig,
+  schema: z.Schema<T>,
+) => {
+  const res = await extApi(config);
+
+  if (res.data?.success) {
+    const data = schema.parse(res.data.data);
+    return data;
+  }
+};
+
+export { extApi, extApiTyped };
