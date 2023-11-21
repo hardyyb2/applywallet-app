b9f366270345d5ec3199d90cb730826bf761741b
hardik badola <hardikb3732@gmail.com>
2023-11-21 22:10:27 +0530

    [UPDATED] calendar styles

--- app/_components/isolated/Calendar/Calendar.tsx
+++ app/_components/isolated/Calendar/Calendar.tsx
@@ -8,6 +8,7 @@ import { cnMerge } from "@/utils/styles";
 
 import { buttonVariants } from "../Button";
 import { Icons } from "../Icons";
+import { typographyVariants } from "../Typography";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
@@ -26,18 +27,20 @@ const Calendar = ({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: typographyVariants({ variant: "subtitle1" }),
         nav: "space-x-1 flex items-center",
         nav_button: cnMerge(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
+          "h-12 w-12 bg-transparent p-0 opacity-50 hover:opacity-100",
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
-        head_cell:
-          "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
+        head_cell: cnMerge(
+          typographyVariants({ variant: "subtitle2" }),
+          "text-base-content/50 rounded-md w-8",
+        ),
         row: "flex w-full mt-2",
         cell: cnMerge(
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50",
@@ -47,18 +50,18 @@ const Calendar = ({
         ),
         day: cnMerge(
           buttonVariants({ color: "ghost" }),
-          "h-8 w-8 p-0 font-normal aria-selected:opacity-100",
+          "h-12 w-12 p-0 font-normal aria-selected:opacity-100",
         ),
         day_range_start: "day-range-start",
         day_range_end: "day-range-end",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-accent text-accent-foreground",
+          "bg-primary text-primary-content hover:bg-primary hover:text-primary-content focus:bg-primary focus:text-primary-content",
+        day_today: "bg-secondary text-secondary-content",
         day_outside:
-          "day-outside text-muted-foreground opacity-50  aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
-        day_disabled: "text-muted-foreground opacity-50",
+          "day-outside opacity-50 aria-selected:bg-accent/50 aria-selected:opacity-30",
+        day_disabled: "opacity-50",
         day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "aria-selected:bg-accent aria-selected:text-accent-content",
         day_hidden: "invisible",
         ...classNames,
       }}
