fa1920980724577549b426cdea4de698747b2ed3
hardik badola <hardikb3732@gmail.com>
2023-11-22 22:22:03 +0530

    [REFACTORED] Input component to support startIcon and endIcon

--- app/_components/isolated/Input/Input.tsx
+++ app/_components/isolated/Input/Input.tsx
@@ -1,6 +1,6 @@
-import { forwardRef, type InputHTMLAttributes } from "react";
+import { forwardRef, type InputHTMLAttributes, type ReactNode } from "react";
 
-import { cn } from "@/utils/styles";
+import { cnMerge } from "@/utils/styles";
 
 import type { InputColorsType, InputSizeTypes } from "./Input.types";
 
@@ -13,6 +13,8 @@ export type InputProps = Omit<
   size?: InputSizeTypes;
   color?: InputColorsType;
   htmlSize?: number;
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
 };
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
@@ -24,40 +26,61 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       color,
       className,
       htmlSize,
+      startIcon = null,
+      endIcon = null,
       ...props
     },
     ref,
   ): JSX.Element => {
+    const classNames = cnMerge(
+      "input",
+      {
+        "input-lg": size === "lg",
+        "input-md": size === "md",
+        "input-sm": size === "sm",
+        "input-xs": size === "xs",
+      },
+      {
+        "input-primary": color === "primary",
+        "input-secondary": color === "secondary",
+        "input-accent": color === "accent",
+        "input-info": color === "info",
+        "input-success": color === "success",
+        "input-warning": color === "warning",
+        "input-error": color === "error",
+        "input-ghost": color === "ghost",
+      },
+      {
+        "focus:outline-offset-0": !borderOffset,
+        "input-bordered": bordered,
+      },
+      className,
+    );
+
+    if (startIcon || endIcon) {
+      return (
+        <div className="relative flex w-full items-center">
+          {startIcon && (
+            <span className="absolute left-4 [&>svg]:w-5">{startIcon}</span>
+          )}
+          <input
+            ref={ref}
+            size={htmlSize}
+            className={cnMerge(classNames, "w-full", {
+              "pl-12": startIcon,
+              "pr-12": endIcon,
+            })}
+            {...props}
+          />
+          {endIcon && (
+            <span className="absolute right-4 [&>svg]:w-5">{endIcon}</span>
+          )}
+        </div>
+      );
+    }
+
     return (
-      <input
-        ref={ref}
-        size={htmlSize}
-        className={cn(
-          "input",
-          {
-            "input-lg": size === "lg",
-            "input-md": size === "md",
-            "input-sm": size === "sm",
-            "input-xs": size === "xs",
-          },
-          {
-            "input-primary": color === "primary",
-            "input-secondary": color === "secondary",
-            "input-accent": color === "accent",
-            "input-info": color === "info",
-            "input-success": color === "success",
-            "input-warning": color === "warning",
-            "input-error": color === "error",
-            "input-ghost": color === "ghost",
-          },
-          {
-            "focus:outline-offset-0": !borderOffset,
-            "input-bordered": bordered,
-          },
-          className,
-        )}
-        {...props}
-      />
+      <input ref={ref} size={htmlSize} className={classNames} {...props} />
     );
   },
 );
