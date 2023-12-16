745465ab2a3b42b6fe5cceef98aab18448e1174d
hardik badola <hardikb3732@gmail.com>
2023-12-16 09:53:26 +0530

    [REFACTOR] date validation and parsing in interviewSchema

--- app/_lib/schema/interviews.ts
+++ app/_lib/schema/interviews.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+import { zodDDMMYYYYDate } from "@/utils/zod";
+
 const roundSchema = z.object({
   name: z.string().min(1, {
     message: "please enter a round name",
@@ -21,58 +23,8 @@ export const interviewSchema = z.object({
   result: z.string().optional(),
   rounds: z.record(roundSchema),
 
-  start_date: z
-    .string()
-    .refine(
-      (dateString) =>
-        /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/((19|20|21)\d\d)$/.test(
-          dateString,
-        ),
-      {
-        message: "please enter a valid date in the format dd/mm/yyyy",
-      },
-    )
-    .transform((dateString, ctx) => {
-      const date = new Date(dateString);
-      if (!z.date().safeParse(date).success) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.invalid_date,
-          message: "please enter a valid date",
-        });
-      }
-      return date;
-    }),
-
-  end_date: z
-    .string()
-    .optional()
-    .refine(
-      (dateString) => {
-        if (!dateString) {
-          return true;
-        }
-        return /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/((19|20|21)\d\d)$/.test(
-          dateString,
-        );
-      },
-      {
-        message: "please enter a valid date in the format dd/mm/yyyy",
-      },
-    )
-    .transform((dateString, ctx) => {
-      if (!dateString) {
-        return;
-      }
-
-      const date = new Date(dateString);
-      if (!z.date().safeParse(date).success) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.invalid_date,
-          message: "please enter a valid date",
-        });
-      }
-      return date;
-    }),
+  start_date: zodDDMMYYYYDate,
+  end_date: zodDDMMYYYYDate.optional(),
 });
 
 /** does not include id */
--- app/_utils/zod.ts
+++ app/_utils/zod.ts
@@ -1,4 +1,8 @@
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import { z } from "zod";
+
+dayjs.extend(customParseFormat);
 
 export const zodKeys = <T extends z.ZodTypeAny>(schema: T): string[] => {
   if (schema === null || schema === undefined) {
@@ -28,3 +32,26 @@ export const zodKeys = <T extends z.ZodTypeAny>(schema: T): string[] => {
 
   return [];
 };
+
+export const zodDDMMYYYYDate = z
+  .string()
+  .refine(
+    (dateString) =>
+      /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(
+        dateString,
+      ),
+    {
+      message: "please enter a valid date in the format dd/mm/yyyy",
+    },
+  )
+  .transform((dateString, ctx) => {
+    const date = dayjs(dateString, "DD/MM/YYYY", true).toDate();
+
+    if (!z.date().safeParse(date).success) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_date,
+        message: "please enter a valid date",
+      });
+    }
+    return date;
+  });
