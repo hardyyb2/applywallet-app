0f93f4a4da0e60f237d7363fcb42c1cf5f0af6cf
hardik badola <hardikb3732@gmail.com>
2023-12-18 22:05:22 +0530

    [ADDED] DropdownMenu component to LocaleSwitcher

--- app/_components/dependent/LocaleSwitcher/LocaleSwitcher.tsx
+++ app/_components/dependent/LocaleSwitcher/LocaleSwitcher.tsx
@@ -4,6 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { Button } from "@/components/isolated/Button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/isolated/DropdownMenu";
 import { Icons } from "@/components/isolated/Icons";
 import {
   Popover,
@@ -11,7 +17,7 @@ import {
   PopoverTrigger,
 } from "@/components/isolated/Popover";
 import { languageOptions } from "@/utils/locale-utils/language-options";
-import { cn } from "@/utils/styles";
+import { cn, cnMerge } from "@/utils/styles";
 
 import { redirectedPathName } from "./localeSwitcher.utils";
 
@@ -19,8 +25,8 @@ const LocaleSwitcher = () => {
   const pathName = usePathname();
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
         <Button
           color="ghost"
           className="gap-1"
@@ -31,41 +37,32 @@ const LocaleSwitcher = () => {
           <Icons.Languages className="h-4 w-4 md:h-5 md:w-5" />
           <Icons.ChevronDown className="ml-1 hidden opacity-60 sm:inline-block" />
         </Button>
-      </PopoverTrigger>
-      <PopoverContent align="end">
-        <ul role="listbox" className="menu gap-2 p-0">
-          {languageOptions.map(({ label, value, icon }) => {
-            const { activeLocale, newRedirectPath } = redirectedPathName(
-              pathName,
-              value,
-            );
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="flex flex-col gap-2">
+        {languageOptions.map(({ label, value, icon }) => {
+          const { activeLocale, newRedirectPath } = redirectedPathName(
+            pathName,
+            value,
+          );
 
-            return (
-              <li
-                key={value}
-                className={cn(
-                  "rounded-lg bg-base-100 outline-offset-2 [&_a]:w-max [&_a]:p-0",
-                  activeLocale === value && "/ bg-primary text-primary-content",
+          return (
+            <Link href={newRedirectPath} key={value}>
+              <DropdownMenuItem
+                className={cnMerge(
+                  "flex gap-3",
+                  activeLocale === value &&
+                    "bg-primary text-primary-content focus:bg-primary",
                 )}
-                role="listitem"
                 aria-label={label}
               >
-                <Link href={newRedirectPath} tabIndex={-1}>
-                  <Button
-                    startIcon={icon}
-                    color="ghost"
-                    className="flex flex-row flex-nowrap"
-                    aria-label={label}
-                  >
-                    <span>{label}</span>
-                  </Button>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </PopoverContent>
-    </Popover>
+                {icon}
+                <span>{label}</span>
+              </DropdownMenuItem>
+            </Link>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
