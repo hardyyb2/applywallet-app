5762d1cd8ecd92a7accc7b661b3b66ff6d9b41f4
hardik badola <hardikb3732@gmail.com>
2023-12-19 10:05:06 +0530

    [UPDATED] DropdownMenuContent alignment

--- app/_components/isolated/DropdownMenu/DropdownMenu.tsx
+++ app/_components/isolated/DropdownMenu/DropdownMenu.tsx
@@ -66,11 +66,12 @@ DropdownMenuSubContent.displayName =
 const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
+>(({ className, sideOffset = 4, align = "end", ...props }, ref) => (
   <DropdownMenuPrimitive.Portal>
     <DropdownMenuPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
+      align={align}
       className={cnMerge(
         "z-50 flex flex-col gap-2 overflow-hidden rounded-xl border border-base-content/40 bg-base-100 p-2xs",
         "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
