f5beb0e9d763548b2c905b8856c24b522f171162
hardik badola <hardikb3732@gmail.com>
2023-12-19 21:24:39 +0530

    [ADDED] useLayoutEffect hook and responsive prop to DatePickerInput

--- app/_components/isolated/DatePicker/DatePickerInput.tsx
+++ app/_components/isolated/DatePicker/DatePickerInput.tsx
@@ -1,35 +1,46 @@
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 
 import dayjs from "dayjs";
 
+import { useBreakPoint } from "@/hooks/useBreakPoint";
 import { cn } from "@/utils/styles";
 
 import { Button } from "../Button";
 import { Calendar } from "../Calendar";
 import { Icons } from "../Icons";
-import { Input } from "../Input";
+import { Input, type InputProps } from "../Input";
 import { Popover, PopoverContent, PopoverTrigger } from "../Popover";
 import type { DatePickerColorType } from "./datePicker.types";
 
-type DatePickerInputProps = {
+interface DatePickerInputProps extends InputProps {
   /** Date in ISO format */
   date: string | undefined;
   setDate: (date: DatePickerInputProps["date"]) => void;
   color?: DatePickerColorType;
-};
+}
 
 const DatePickerInput = ({
   date,
   setDate,
   color = "ghost",
+  responsive = true,
+  ...props
 }: DatePickerInputProps) => {
   // we do not accept date object to keep frontend and backend consistent
   const isValidDate = typeof date === "string" && dayjs(date).isValid();
   const dateObj = isValidDate ? dayjs(date).toDate() : undefined;
 
+  // hooks & states
+  const { isAboveSm } = useBreakPoint("sm");
   const [dateInputValue, setDateInputValue] = useState<string | undefined>(
     date ? dayjs(date).format("YYYY-MM-DD") : "",
   );
+  const [isAbSm, setIsAbSm] = useState(false);
+
+  // functions
+  useLayoutEffect(() => {
+    setIsAbSm(isAboveSm);
+  }, [isAboveSm]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -49,36 +60,42 @@ const DatePickerInput = ({
   return (
     <Popover>
       <Input
+        placeholder="dd/mm/yyyy"
+        responsive={responsive}
+        iconWrapperClassName="w-full h-full"
+        {...props}
+        iconSeparate
         color={color}
         type="date"
         value={dateInputValue}
         onChange={handleInputChange}
-        placeholder="dd/mm/yyyy"
-        iconSeparate
-        iconWrapperClassName="w-full h-full"
         endIcon={
-          <PopoverTrigger asChild>
-            <Button
-              color={color}
-              variant="outline"
-              className="w-20 p-0 hover:!bg-transparent"
-              startIcon={
-                <Icons.Calendar
-                  className={cn(
-                    "cursor-pointer",
-                    color === "primary" && "stroke-primary",
-                    color === "secondary" && "stroke-secondary",
-                    color === "accent" && "stroke-accent",
-                    color === "ghost" && "stroke-base-content",
-                    color === "error" && "stroke-error",
-                    color === "warning" && "stroke-warning",
-                    color === "success" && "stroke-success",
-                    color === "info" && "stroke-info",
-                  )}
-                />
-              }
-            />
-          </PopoverTrigger>
+          isAbSm ? (
+            <PopoverTrigger asChild>
+              <Button
+                color={color}
+                variant="outline"
+                responsive={responsive}
+                className="w-20 p-0 hover:!bg-transparent"
+                startIcon={
+                  <Icons.Calendar
+                    className={cn(
+                      "cursor-pointer",
+                      color === "primary" && "stroke-primary",
+                      color === "secondary" && "stroke-secondary",
+                      color === "accent" && "stroke-accent",
+                      color === "ghost" && "stroke-base-content",
+                      color === "error" && "stroke-error",
+                      color === "warning" && "stroke-warning",
+                      color === "success" && "stroke-success",
+                      color === "info" && "stroke-info",
+                      responsive && "h-4 w-4 md:h-6 md:w-6",
+                    )}
+                  />
+                }
+              />
+            </PopoverTrigger>
+          ) : null
         }
       />
 
