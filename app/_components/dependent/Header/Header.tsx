ec0984b70cf0f74f5367f95912767aa23ad0ec4b
hardik badola <hardikb3732@gmail.com>
2023-12-19 10:04:46 +0530

    [REFACTORED] Header component to use Flex layout

--- app/_components/dependent/Header/Header.tsx
+++ app/_components/dependent/Header/Header.tsx
@@ -1,15 +1,28 @@
+import { Flex } from "@/components/isolated/Flex";
+import { Typography } from "@/components/isolated/Typography";
+import { cnMerge } from "@/utils/styles";
+
 import { LocaleSwitcher } from "../LocaleSwitcher";
 import { ThemesMenu } from "../ThemesMenu";
 import { UserMenu } from "../UserMenu";
-import { HeaderClient } from "./HeaderClient";
 
 const Header = async () => {
   return (
-    <HeaderClient>
-      <ThemesMenu />
-      <LocaleSwitcher />
-      <UserMenu />
-    </HeaderClient>
+    <Flex
+      align="center"
+      justify="space-between"
+      className={cnMerge("h-full w-full px-4 lg:px-10")}
+    >
+      <div className="text-lg inline-flex lowercase text-primary">
+        <Typography>apply</Typography>
+        <Typography className="text-base-content">wallet</Typography>
+      </div>
+      <Flex align="center" className="gap-1 lg:gap-2">
+        <ThemesMenu />
+        <LocaleSwitcher />
+        <UserMenu />
+      </Flex>
+    </Flex>
   );
 };
 
