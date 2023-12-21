8b32f1c438d3549d34d8c864f2609fb4fb79ca27
hardik badola <hardikb3732@gmail.com>
2023-12-21 19:23:04 +0530

    [REFACTORED] Accordion component to use context and add variant support

--- app/_components/isolated/Accordion/Accordion.tsx
+++ app/_components/isolated/Accordion/Accordion.tsx
@@ -8,19 +8,75 @@ import { type LucideIcon } from "lucide-react";
 import { cnMerge } from "@/utils/styles";
 
 import { Icons } from "../Icons";
+import { typographyVariants } from "../Typography";
 
-const Accordion = AccordionPrimitive.Root;
+type AccordionContextType = {
+  variant?: "primary" | "secondary";
+};
+
+const AccordionContext = React.createContext<AccordionContextType>({
+  variant: "primary",
+});
+
+const useAccordionContext = () => {
+  const context = React.useContext(AccordionContext);
+  if (!context) {
+    throw new Error(
+      "useAccordionContext must be used within a AccordionContextProvider",
+    );
+  }
+  return context;
+};
+
+type AccordionProps = React.ComponentPropsWithoutRef<
+  typeof AccordionPrimitive.Root
+> &
+  AccordionContextType;
+
+const Accordion = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Root>,
+  AccordionProps
+>(({ variant = "primary", className, ...props }, ref) => {
+  return (
+    <AccordionContext.Provider value={{ variant }}>
+      <AccordionPrimitive.Root
+        ref={ref}
+        {...props}
+        className={cnMerge(
+          {
+            "space-y-1": variant === "primary",
+          },
+          className,
+        )}
+      />
+    </AccordionContext.Provider>
+  );
+});
+
+Accordion.displayName = "Accordion";
 
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
->(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item
-    ref={ref}
-    className={cnMerge("border-b text-h5", className)}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const { variant } = useAccordionContext();
+
+  return (
+    <AccordionPrimitive.Item
+      ref={ref}
+      className={cnMerge(
+        typographyVariants({ variant: "label-m" }),
+        "p-2xs-xs ",
+        {
+          "rounded-xl bg-base-200 ": variant === "primary",
+          "border-b": variant === "secondary",
+        },
+        className,
+      )}
+      {...props}
+    />
+  );
+});
 AccordionItem.displayName = "AccordionItem";
 
 const AccordionTrigger = React.forwardRef<
@@ -44,13 +100,13 @@ const AccordionTrigger = React.forwardRef<
       <AccordionPrimitive.Trigger
         ref={ref}
         className={cnMerge(
-          "flex flex-1 items-center justify-between py-4 text-body1 font-medium",
+          "flex flex-1 items-center justify-between",
           className,
         )}
         {...props}
       >
         {children}
-        {showIcon ? <Icon /> : null}
+        {showIcon ? <Icon className="h-4 w-4 lg:h-6 lg:w-6" /> : null}
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   ),
@@ -62,18 +118,26 @@ const AccordionContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content> & {
     innerClassName?: string;
   }
->(({ className, innerClassName, children, ...props }, ref) => (
-  <AccordionPrimitive.Content
-    ref={ref}
-    className={cnMerge(
-      "overflow-hidden text-body1 data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
-      className,
-    )}
-    {...props}
-  >
-    <div className={cnMerge("pb-4 pt-0", innerClassName)}>{children}</div>
-  </AccordionPrimitive.Content>
-));
+>(({ className, innerClassName, children, ...props }, ref) => {
+  const { variant } = useAccordionContext();
+
+  return (
+    <AccordionPrimitive.Content
+      ref={ref}
+      className={cnMerge(
+        "overflow-hidden",
+        {
+          "p-3xs-2xs": variant === "primary",
+        },
+        "data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
+        className,
+      )}
+      {...props}
+    >
+      <div className={cnMerge("pb-4 pt-0", innerClassName)}>{children}</div>
+    </AccordionPrimitive.Content>
+  );
+});
 AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
