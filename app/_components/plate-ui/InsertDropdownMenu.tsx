af92ecc097da040450c579f8e5c1704544351f60
hardik badola <hardikb3732@gmail.com>
2023-12-22 18:27:08 +0530

    [ADDED] InsertDropdownMenu component

---
+++ app/_components/plate-ui/InsertDropdownMenu.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import React from "react";
+
+import { type DropdownMenuProps } from "@radix-ui/react-dropdown-menu";
+import {
+  focusEditor,
+  insertEmptyElement,
+  useEditorState,
+} from "@udecode/plate-common";
+import {
+  ELEMENT_H1,
+  ELEMENT_H2,
+  ELEMENT_H3,
+  ELEMENT_H4,
+  ELEMENT_H5,
+  ELEMENT_H6,
+} from "@udecode/plate-heading";
+import { ELEMENT_PARAGRAPH } from "@udecode/plate-paragraph";
+import { useBoolean } from "react-use";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../isolated/DropdownMenu";
+import { Icons } from "../isolated/Icons";
+import { ToolbarButton } from "../isolated/Toolbar";
+
+const items = [
+  {
+    label: "Basic blocks",
+    items: [
+      {
+        value: ELEMENT_PARAGRAPH,
+        label: "Paragraph",
+        description: "Paragraph",
+        icon: Icons.Pilcrow,
+      },
+      {
+        value: ELEMENT_H1,
+        label: "Heading 1",
+        description: "Heading 1",
+        icon: Icons.Heading1,
+      },
+      {
+        value: ELEMENT_H2,
+        label: "Heading 2",
+        description: "Heading 2",
+        icon: Icons.Heading2,
+      },
+      {
+        value: ELEMENT_H3,
+        label: "Heading 3",
+        description: "Heading 3",
+        icon: Icons.Heading3,
+      },
+      {
+        value: ELEMENT_H4,
+        label: "Heading 4",
+        description: "Heading 4",
+        icon: Icons.Heading4,
+      },
+      {
+        value: ELEMENT_H5,
+        label: "Heading 5",
+        description: "Heading 5",
+        icon: Icons.Heading5,
+      },
+      {
+        value: ELEMENT_H6,
+        label: "Heading 6",
+        description: "Heading 6",
+        icon: Icons.Heading6,
+      },
+    ],
+  },
+];
+
+export function InsertDropdownMenu(props: DropdownMenuProps) {
+  const editor = useEditorState();
+  // const openState = useOpenState();
+  const [open, setOpen] = useBoolean(false);
+
+  return (
+    <DropdownMenu modal={false} open={open} onOpenChange={setOpen} {...props}>
+      <DropdownMenuTrigger asChild>
+        <ToolbarButton pressed={open} tooltip="Insert" isDropdown>
+          <Icons.Plus />
+        </ToolbarButton>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        align="start"
+        className="flex max-h-[500px] min-w-0 flex-col gap-0.5 overflow-y-auto"
+      >
+        {items.map(({ items: nestedItems, label }, index) => (
+          <React.Fragment key={label}>
+            {index !== 0 && <DropdownMenuSeparator />}
+
+            <DropdownMenuLabel>{label}</DropdownMenuLabel>
+            {nestedItems.map(
+              ({ value: type, label: itemLabel, icon: Icon }) => (
+                <DropdownMenuItem
+                  key={type}
+                  className="min-w-[180px]"
+                  onSelect={async () => {
+                    switch (type) {
+                      default: {
+                        insertEmptyElement(editor, type, {
+                          select: true,
+                          nextBlock: true,
+                        });
+                      }
+                    }
+
+                    focusEditor(editor);
+                  }}
+                >
+                  <Icon className="mr-2 h-5 w-5" />
+                  {itemLabel}
+                </DropdownMenuItem>
+              ),
+            )}
+          </React.Fragment>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
