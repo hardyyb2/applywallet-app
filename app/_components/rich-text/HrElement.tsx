4e6f7ac55af7b3b735dba8c3d844bb32994e44f6
hardik badola <hardikb3732@gmail.com>
2023-12-23 18:20:16 +0530

    [ADDED] HrElement component to rich-text module

---
+++ app/_components/rich-text/HrElement.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { PlateElement, type PlateElementProps } from "@udecode/plate-common";
+import { useFocused, useSelected } from "slate-react";
+
+import { cnM } from "@/utils/styles";
+
+const HrElement = React.forwardRef<
+  React.ElementRef<typeof PlateElement>,
+  PlateElementProps
+>(({ className, nodeProps, ...props }, ref) => {
+  const { children } = props;
+
+  const selected = useSelected();
+  const focused = useFocused();
+
+  return (
+    <PlateElement ref={ref} {...props}>
+      <div className="py-6" contentEditable={false}>
+        <hr
+          {...nodeProps}
+          className={cnM(
+            "h-0.5 cursor-pointer rounded-sm border-none bg-base-content bg-clip-content",
+            selected && focused && "ring-2 ring-base-content/40 ring-offset-2",
+            className,
+          )}
+        />
+      </div>
+      {children}
+    </PlateElement>
+  );
+});
+HrElement.displayName = "HrElement";
+
+export { HrElement };
