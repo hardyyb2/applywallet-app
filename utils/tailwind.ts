4448bd1ef43026c3b8064ce56d2c64b54c50bb1f
hardik badola <hardikb3732@gmail.com>
2023-12-24 11:03:46 +0530

    [ADDED] typography component for caption-m

--- utils/tailwind.ts
+++ utils/tailwind.ts
@@ -152,6 +152,15 @@ const typographyComponentsPlugin = plugin(({ addComponents, theme }) => {
       letterSpacing: "0.5pt",
       fontFamily: theme("fontFamily.secondary"),
     },
+
+    ".caption-m": {
+      fontSize: "9pt", // 12px
+      fontWeight: "400",
+      fontStyle: "normal",
+      lineHeight: "12pt", // 16px
+      letterSpacing: "0.4pt",
+      fontFamily: theme("fontFamily.secondary"),
+    },
   });
 });
 
