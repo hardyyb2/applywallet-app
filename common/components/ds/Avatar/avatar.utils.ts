a640fc9e0b8be1899e51f9e0be8d116bb0065c24
hardik badola <hardikb3732@gmail.com>
2023-12-25 10:19:40 +0530

    [REFACTORED] avatarVariants in avatar.utils.ts

--- common/components/ds/Avatar/avatar.utils.ts
+++ common/components/ds/Avatar/avatar.utils.ts
@@ -1,54 +1,61 @@
 import { cva } from "cva";
 
-export const avatarVariants = cva("dui-avatar overflow-hidden", {
-  variants: {
-    size: {
-      xl: "w-xl",
-      lg: "w-l",
-      md: "w-m",
-      sm: "w-s",
-      xs: "w-xs",
+export const avatarVariants = cva(
+  "dui-avatar overflow-hidden shrink-0 grow-0 aspect-square",
+  {
+    variants: {
+      shape: {
+        square: "rounded-xl",
+        circle: "rounded-full",
+      },
+      size: {
+        xl: "w-xl rounded-2xl",
+        lg: "w-l rounded-xl",
+        md: "w-m rounded-lg",
+        sm: "w-s rounded-md",
+        xs: "w-xs rounded-sm",
+      },
+      color: {
+        primary: "bg-primary text-primary-content",
+        secondary: "bg-secondary text-secondary-content",
+        accent: "bg-accent text-accent-content",
+        neutral: "bg-neutral text-neutral-content",
+      },
+      border: {
+        true: "ring ring-offset-2",
+      },
     },
-    shape: {
-      circle: "rounded-full",
-      square: "rounded-xl",
-    },
-    color: {
-      primary: "bg-primary text-primary-content",
-      secondary: "bg-secondary text-secondary-content",
-      accent: "bg-accent text-accent-content",
-      neutral: "bg-neutral text-neutral-content",
-    },
-    border: {
-      true: "ring ring-offset-2",
-    },
-  },
-  compoundVariants: [
-    {
-      border: true,
-      color: "primary",
-      className: "ring-primary",
-    },
-    {
-      border: true,
-      color: "secondary",
-      className: "ring-secondary",
-    },
-    {
-      border: true,
-      color: "accent",
-      className: "ring-accent",
-    },
-    {
-      border: true,
+    compoundVariants: [
+      {
+        shape: "circle",
+        className: "rounded-full",
+      },
+      {
+        border: true,
+        color: "primary",
+        className: "ring-primary",
+      },
+      {
+        border: true,
+        color: "secondary",
+        className: "ring-secondary",
+      },
+      {
+        border: true,
+        color: "accent",
+        className: "ring-accent",
+      },
+      {
+        border: true,
+        color: "neutral",
+        className: "ring-neutral",
+      },
+    ],
+    defaultVariants: {
+      size: "lg",
+      shape: "square",
       color: "neutral",
-      className: "ring-neutral",
+      border: true,
     },
-  ],
-  defaultVariants: {
-    size: "lg",
-    shape: "square",
-    color: "neutral",
-    border: true,
   },
-});
+);
