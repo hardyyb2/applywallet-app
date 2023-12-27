432e33c8ef177a1ae5ef43507c876597c9447b91
hardik badola <hardikb3732@gmail.com>
2023-12-27 21:13:36 +0530

    [REMOVED] debug flag from Sentry configuration files

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,5 +7,4 @@ const isDev = envVars.NODE_ENV === "development";
 Sentry.init({
   dsn: "https://3f61abf4d77bb0cfbe6a2aef0a8cae07@o4506184356003840.ingest.sentry.io/4506184358035456",
   tracesSampleRate: isDev ? 1 : 0.2,
-  debug: isDev,
 });
