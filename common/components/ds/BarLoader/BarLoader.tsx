a702925a959bb88f5b23daf0bdccfb392e5fdf8a
hardik badola <hardikb3732@gmail.com>
2023-12-29 21:59:10 +0530

    [REFACTORED] BarLoader component from framer to daisyui

--- common/components/ds/BarLoader/BarLoader.tsx
+++ common/components/ds/BarLoader/BarLoader.tsx
@@ -1,66 +1,33 @@
 "use client";
 
-import { m, type HTMLMotionProps, type Variants } from "framer-motion";
+import type { HTMLAttributes } from "react";
 
 import { cnM } from "@/utils/styles";
 
-interface BarLoaderProps extends HTMLMotionProps<"div"> {
-  barClassName?: string;
+interface BarLoaderProps extends HTMLAttributes<HTMLDivElement> {
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
 }
 
-const variants: Variants = {
-  initial: {
-    scaleY: 0.5,
-    opacity: 0,
-  },
-  animate: {
-    scaleY: 1,
-    opacity: 1,
-    transition: {
-      repeat: Infinity,
-      repeatType: "mirror",
-      duration: 1,
-      ease: "circIn",
-    },
-  },
-};
-
 const BarLoader = ({
-  barClassName = "",
   className = "",
+  size = "xl",
   ...rest
 }: BarLoaderProps) => {
   return (
-    <m.div
-      transition={{
-        staggerChildren: 0.25,
-      }}
-      initial="initial"
-      animate="animate"
+    <div
       {...rest}
-      className={cnM("flex h-full w-full gap-1", className)}
-    >
-      <m.div
-        variants={variants}
-        className={cnM("h-2xl w-xs bg-base-content", barClassName)}
-      />
-      <m.div
-        variants={variants}
-        className={cnM("h-2xl w-xs bg-base-content", barClassName)}
-      />
-      <m.div
-        variants={variants}
-        className={cnM("h-2xl w-xs bg-base-content", barClassName)}
-      />
-      <m.div
-        variants={variants}
-        className={cnM("h-2xl w-xs bg-base-content", barClassName)}
-      />
-      <m.div
-        variants={variants}
-        className={cnM("h-2xl w-xs bg-base-content", barClassName)}
-      />
-    </m.div>
+      className={cnM(
+        "dui-loading dui-loading-bars",
+        {
+          "dui-loading-xs": size === "xs",
+          "dui-loading-sm": size === "sm",
+          "dui-loading-md": size === "md",
+          "dui-loading-lg": size === "lg",
+          "dui-loading-xl": size === "xl",
+        },
+        className,
+      )}
+    />
   );
 };
 
