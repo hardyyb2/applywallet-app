1d75455ae3da1a220394bc37cd53d860102edbc8
hardik badola <hardikb3732@gmail.com>
2023-12-30 09:00:29 +0530

    [REFACTORED] BlogCardMobile component layout and styles

--- app/(routes)/[locale]/(nav-layout)/(docs)/blogs/BlogCard/BlogCardMobile.tsx
+++ app/(routes)/[locale]/(nav-layout)/(docs)/blogs/BlogCard/BlogCardMobile.tsx
@@ -18,40 +18,39 @@ const BlogCardMobile = ({
   tags,
 }: BlogCardMobileProps) => {
   return (
-    <Card className={cn("group max-w-none bg-base-100")}>
-      Mobile
-      {image ? (
-        <Card.Figure className={cn("m-3xs h-80")}>
-          <Image
-            className="transition-all duration-500 group-hover:scale-110"
-            objectFit="cover"
-            src={image}
-            alt={image}
-            fill
-          />
-        </Card.Figure>
-      ) : null}
-      <Card.Body className="rounded-t-none">
-        <span className="label-s line-clamp-1 text-secondary lg:label-m">
-          {category.name}
-        </span>
-        <Card.Title className="headline-s mb-2xs-xs line-clamp-4">
-          {title}
-        </Card.Title>
+    <Card className={cn("group max-w-none")}>
+      <Card.Body>
+        <div className="grid grid-cols-[1fr_120px] gap-xs">
+          <Flex direction="column">
+            <span className="label-xs line-clamp-1 text-secondary lg:label-m">
+              {category.name}
+            </span>
+            <Card.Title className="title-s mb-2xs-xs line-clamp-2 md:title-m">
+              {title}
+            </Card.Title>
 
-        <span className="body-s mb-3xs-2xs line-clamp-3 font-light">
-          {description}
-        </span>
-
-        {tags?.length ? (
-          <Flex className="gap-2">
-            {tags.map((tag) => (
-              <Badge variant="outline" size="lg" key={tag}>
-                {tag}
-              </Badge>
-            ))}
+            {tags?.length ? (
+              <Flex className="gap-2">
+                {tags.map((tag) => (
+                  <Badge variant="outline" size="sm" key={tag}>
+                    {tag}
+                  </Badge>
+                ))}
+              </Flex>
+            ) : null}
           </Flex>
-        ) : null}
+          {image ? (
+            <Card.Figure className={cn(" aspect-square rounded-lg")}>
+              <Image
+                className="transition-all duration-500 group-hover:scale-110"
+                objectFit="cover"
+                src={image}
+                alt={image}
+                fill
+              />
+            </Card.Figure>
+          ) : null}
+        </div>
       </Card.Body>
     </Card>
   );
