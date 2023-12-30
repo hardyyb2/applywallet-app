b944e8216fdfed5b2025a5b7797371459c4401d1
hardik badola <hardikb3732@gmail.com>
2023-12-30 09:06:45 +0530

    [ADDED] responsive layout for BlogCardsWrapper

--- app/(routes)/[locale]/(nav-layout)/(docs)/blogs/[...slug]/BlogCardsWrapper.tsx
+++ app/(routes)/[locale]/(nav-layout)/(docs)/blogs/[...slug]/BlogCardsWrapper.tsx
@@ -4,7 +4,15 @@ import type { PropsWithChildren } from "react";
 
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
+import { useBreakPoint } from "@/hooks/useBreakPoint";
+
 const BlogCardsWrapper = ({ children }: PropsWithChildren) => {
+  const { isBelowLg } = useBreakPoint("lg");
+
+  if (isBelowLg) {
+    return <div className="grid grid-cols-1 gap-xs">{children}</div>;
+  }
+
   return (
     <ResponsiveMasonry
       columnsCountBreakPoints={{ 300: 1, 768: 2, 1440: 3, 1920: 4 }}
