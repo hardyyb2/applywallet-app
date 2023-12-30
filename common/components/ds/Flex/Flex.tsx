df78ebdfafb1d5917599e77aa696c5a1c8c03ff7
hardik badola <hardikb3732@gmail.com>
2023-12-30 11:03:14 +0530

    [REFACTORED] Flex component to use cva for variants

--- common/components/ds/Flex/Flex.tsx
+++ common/components/ds/Flex/Flex.tsx
@@ -6,25 +6,15 @@ import {
   type ReactNode,
 } from "react";
 
-import { cn } from "@/utils/styles";
+import type { VariantProps } from "cva";
 
-import type {
-  FlexAlignType,
-  FlexDirectionType,
-  FlexJustifyType,
-  FlexScreensType,
-  FlexWrapType,
-} from "./flex.types";
+import { flexVariants } from "./flex.utils";
 
-export interface FlexProps extends HTMLAttributes<HTMLDivElement> {
-  direction?: FlexDirectionType;
-  align?: FlexAlignType;
-  justify?: FlexJustifyType;
-  wrap?: FlexWrapType;
+export interface FlexProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof flexVariants> {
   component?: keyof JSX.IntrinsicElements | ComponentType<any>;
-  className?: string;
   children?: ReactNode | ReactNode[];
-  switchDirection?: FlexScreensType | false;
 }
 
 const Flex = forwardRef<
@@ -40,58 +30,19 @@ const Flex = forwardRef<
       component: Component = "div",
       className = "",
       children = null,
-      switchDirection = false,
     },
     ref,
   ) => {
     return (
       <Component
         ref={ref}
-        className={cn(
-          "flex",
-          {
-            "flex-row": direction === "row",
-            "flex-row-reverse": direction === "row-reverse",
-            "flex-col": direction === "column",
-            "flex-col-reverse": direction === "column-reverse",
-          },
-          {
-            // shouldn't have made this component :(
-            "sm:flex-col": direction === "row" && switchDirection === "sm",
-            "sm:flex-row": direction === "column" && switchDirection === "sm",
-            "md:flex-col": direction === "row" && switchDirection === "md",
-            "md:flex-row": direction === "column" && switchDirection === "md",
-            "lg:flex-col": direction === "row" && switchDirection === "lg",
-            "lg:flex-row": direction === "column" && switchDirection === "lg",
-            "xl:flex-col": direction === "row" && switchDirection === "xl",
-            "xl:flex-row": direction === "column" && switchDirection === "xl",
-            "2xl:flex-col": direction === "row" && switchDirection === "2xl",
-            "2xl:flex-row": direction === "column" && switchDirection === "2xl",
-            "3xl:flex-col": direction === "row" && switchDirection === "3xl",
-            "3xl:flex-row": direction === "column" && switchDirection === "3xl",
-            "4xl:flex-col": direction === "row" && switchDirection === "4xl",
-            "4xl:flex-row": direction === "column" && switchDirection === "4xl",
-          },
-          {
-            "items-stretch": align === "stretch",
-            "items-start": align === "flex-start",
-            "items-end": align === "flex-end",
-            "items-center": align === "center",
-          },
-          {
-            "justify-start": justify === "flex-start",
-            "justify-end": justify === "flex-end",
-            "justify-center": justify === "center",
-            "justify-between": justify === "space-between",
-            "justify-around": justify === "space-around",
-          },
-          {
-            "flex-wrap": wrap === "wrap",
-            "flex-nowrap": wrap === "nowrap",
-            "flex-wrap-reverse": wrap === "wrap-reverse",
-          },
+        className={flexVariants({
+          direction,
+          align,
+          justify,
+          wrap,
           className,
-        )}
+        })}
       >
         {children}
       </Component>
---
+++ common/components/ds/Flex/flex.utils.ts
@@ -0,0 +1,37 @@
+import { cva } from "cva";
+
+const flexVariants = cva("flex", {
+  variants: {
+    direction: {
+      row: "flex-row",
+      "row-reverse": "flex-row-reverse",
+      column: "flex-col",
+      "column-reverse": "flex-col-reverse",
+    },
+    align: {
+      stretch: "items-stretch",
+      "flex-start": "items-start",
+      "flex-end": "items-end",
+      center: "items-center",
+    },
+    justify: {
+      "flex-start": "justify-start",
+      "flex-end": "justify-end",
+      center: "justify-center",
+      "space-between": "justify-between",
+      "space-around": "justify-around",
+    },
+    wrap: {
+      wrap: "flex-wrap",
+      nowrap: "flex-nowrap",
+      "wrap-reverse": "flex-wrap-reverse",
+    },
+  },
+  defaultVariants: {
+    align: "stretch",
+    justify: "flex-start",
+    wrap: "nowrap",
+  },
+});
+
+export { flexVariants };
