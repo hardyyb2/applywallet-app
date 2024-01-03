07f4574aca7c8e346ea221f269d1481475c5fcf5
hardik badola <hardikb3732@gmail.com>
2024-01-03 22:14:11 +0530

    [ADDED] DropdownMenuPortal to ThemesMenu component

--- app/_components/dependent/ThemesMenu/ThemesMenu.tsx
+++ app/_components/dependent/ThemesMenu/ThemesMenu.tsx
@@ -5,6 +5,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuPortal,
   DropdownMenuTrigger,
 } from "~/components/ds/DropdownMenu";
 import { Icons } from "~/components/ds/Icons";
@@ -27,31 +28,36 @@ const ThemesMenu = () => {
           <Icons.ChevronDown className="ml-1 hidden opacity-60 sm:inline-block" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent role="listbox" className="max-h-80 overflow-y-auto">
-        {selectableThemes.map(({ value }) => (
-          <DropdownMenuItem key={value} asChild>
-            <Button
-              data-theme={value}
-              onClick={() => setTheme(value)}
-              className={cn(
-                activeTheme === value &&
-                  "outline outline-offset-2 outline-base-content",
-                "flex justify-between gap-2",
-              )}
-              startIcon={
-                activeTheme === value ? (
-                  <Icons.Check className="h-4 w-4 stroke-current stroke-[4px]" />
-                ) : null
-              }
-            >
-              <span className="label-s text-base-content lg:label-m">
-                {value}
-              </span>
-              <ThemesMenuItemColors />
-            </Button>
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
+      <DropdownMenuPortal>
+        <DropdownMenuContent
+          role="listbox"
+          className="max-h-80 overflow-y-auto"
+        >
+          {selectableThemes.map(({ value }) => (
+            <DropdownMenuItem key={value} asChild>
+              <Button
+                data-theme={value}
+                onClick={() => setTheme(value)}
+                className={cn(
+                  activeTheme === value &&
+                    "outline outline-offset-2 outline-base-content",
+                  "flex justify-between gap-2",
+                )}
+                startIcon={
+                  activeTheme === value ? (
+                    <Icons.Check className="h-4 w-4 stroke-current stroke-[4px]" />
+                  ) : null
+                }
+              >
+                <span className="label-s text-base-content lg:label-m">
+                  {value}
+                </span>
+                <ThemesMenuItemColors />
+              </Button>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
     </DropdownMenu>
   );
 };
