6fe76e0be3edc0df4373d74758242310ffc23fb5
hardik badola <hardikb3732@gmail.com>
2024-01-03 09:52:46 +0530

    [UPDATED] retry option for queries to 2

--- app/(routes)/[locale]/providers.tsx
+++ app/(routes)/[locale]/providers.tsx
@@ -32,6 +32,7 @@ const Providers = ({ children, locale }: ProvidersType) => {
       new QueryClient({
         defaultOptions: {
           queries: {
+            retry: 2,
             staleTime: 5 * 60 * 1000, // 5 minutes
             gcTime: 15 * 60 * 1000, // 15 minutes
           },
