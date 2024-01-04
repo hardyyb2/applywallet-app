c735859269181a3b44514ea81033797828a5e975
hardik badola <hardikb3732@gmail.com>
2024-01-04 17:07:13 +0530

    [ADDED] dropdown menu with edit option to InterviewsTable

--- app/(routes)/[locale]/(nav-layout)/(swr)/interviews/InterviewsTable.tsx
+++ app/(routes)/[locale]/(nav-layout)/(swr)/interviews/InterviewsTable.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useMemo, useState } from "react";
+import Link from "next/link";
 
 import {
   flexRender,
@@ -13,6 +14,13 @@ import {
 import dayjs from "dayjs";
 
 import { BarLoader } from "~/components/ds/BarLoader";
+import { Button } from "~/components/ds/Button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/components/ds/DropdownMenu";
 import { Flex } from "~/components/ds/Flex";
 import { Icons } from "~/components/ds/Icons";
 import {
@@ -23,6 +31,7 @@ import {
   TableHeader,
   TableRow,
 } from "~/components/ds/Table";
+import { AppRoutes } from "~/utils/routes";
 
 import type { InterviewType } from "@/lib/schema/interviews";
 import { useInterviews } from "@/queries/interviews.queries";
@@ -85,6 +94,36 @@ const tableColumns: ColumnDef<InterviewType>[] = [
     },
     enableSorting: false,
   },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const interview = row.original;
+
+      return (
+        <DropdownMenu>
+          <div className="w-full text-right">
+            <DropdownMenuTrigger asChild>
+              <Button color="ghost" responsive>
+                <span className="sr-only">open menu</span>
+                <Icons.MoreHorizontal />
+              </Button>
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem asChild>
+              <Link
+                href={AppRoutes.editInterview(interview.id)}
+                className="gap-2"
+              >
+                <Icons.Pencil className="w-4 lg:w-5" />
+                edit
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
 ];
 
 type InterviewsTableProps = {};
@@ -142,7 +181,7 @@ const InterviewsTable = (props: InterviewsTableProps) => {
                           header.column.columnDef.header,
                           header.getContext(),
                         )}
-                        <Icons.ChevronsUpDown className="h-4" />
+                        <Icons.ChevronsUpDown className="h-4 w-4" />
                       </Flex>
                     ) : (
                       flexRender(
@@ -164,7 +203,10 @@ const InterviewsTable = (props: InterviewsTableProps) => {
               hoverable
             >
               {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>
+                <TableCell
+                  key={cell.id}
+                  className="max-w-32 overflow-hidden overflow-ellipsis whitespace-nowrap lg:max-w-60"
+                >
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </TableCell>
               ))}
