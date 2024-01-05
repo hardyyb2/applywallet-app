2231037f21dc532e62802090bf2b2fe0c9e72e72
hardik badola <hardikb3732@gmail.com>
2024-01-05 18:21:27 +0530

    [REFACTORED] InterviewsTable component

--- app/(routes)/[locale]/(nav-layout)/(swr)/interviews/InterviewsTable.tsx
+++ app/(routes)/[locale]/(nav-layout)/(swr)/interviews/InterviewsTable.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import Link from "next/link";
 
+import { useQueryClient } from "@tanstack/react-query";
 import {
   flexRender,
   getCoreRowModel,
@@ -12,6 +13,7 @@ import {
   type SortingState,
 } from "@tanstack/react-table";
 import dayjs from "dayjs";
+import { toast } from "react-toastify";
 
 import { BarLoader } from "~/components/ds/BarLoader";
 import { Button } from "~/components/ds/Button";
@@ -31,128 +33,177 @@ import {
   TableHeader,
   TableRow,
 } from "~/components/ds/Table";
-import { AppRoutes } from "~/utils/routes";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ds/Tooltip";
+import { ApiRoutes, AppRoutes } from "~/utils/routes";
 
+import { appApi } from "@/lib/app-api";
 import type { InterviewType } from "@/lib/schema/interviews";
 import { useInterviews } from "@/queries/interviews.queries";
+import { QueryKeys } from "@/utils/queries";
 
 import {
   interviewResultOptionsMap,
   interviewStatusOptionsMap,
 } from "./interview.utils";
 
-const tableColumns: ColumnDef<InterviewType>[] = [
-  {
-    accessorKey: "status",
-    header: "status",
-    cell: ({ row }) => {
-      const status = row.original.status;
-      return interviewStatusOptionsMap[status].icon;
-    },
-  },
-  {
-    accessorKey: "company_name",
-    header: "company name",
-  },
-  {
-    accessorKey: "position",
-    header: "position",
-    enableSorting: false,
-  },
-  {
-    accessorKey: "rounds",
-    header: "rounds",
-    cell: ({ row }) => {
-      const rounds = row.original.rounds;
-      return rounds.length;
-    },
-  },
-  {
-    accessorKey: "start_date",
-    header: "start_date",
-    cell: ({ row }) => {
-      return dayjs(row.original.start_date).format("DD/MM/YYYY");
-    },
-    sortingFn: (rowA, rowB) => {
-      const startDateA = rowA.original.start_date;
-      const startDateB = rowB.original.start_date;
-
-      if (!dayjs(startDateA).isValid() || !dayjs(startDateB).isValid()) {
-        return 0;
-      }
-
-      return dayjs(startDateA).diff(dayjs(startDateB));
-    },
-  },
-  {
-    accessorKey: "end_date",
-    header: "end date",
-    cell: ({ row }) => {
-      const endDate: string = row.getValue("end_date");
-
-      if (!endDate || !dayjs(endDate).isValid()) {
-        return "-";
-      }
-
-      return dayjs(row.getValue("end_date")).format("DD/MM/YYYY");
-    },
-  },
-  {
-    accessorKey: "result",
-    header: "result",
-    cell: ({ row }) => {
-      const result = row.original.result;
-
-      if (!result) {
-        return "-";
-      }
-
-      const option = interviewResultOptionsMap[result];
-      return (
-        <Flex className="gap-3xs" align="center">
-          {option.icon} {option.label}
-        </Flex>
-      );
-    },
-    enableSorting: false,
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      const interview = row.original;
-
-      return (
-        <DropdownMenu>
-          <div className="w-full text-right">
-            <DropdownMenuTrigger asChild>
-              <Button color="ghost" responsive>
-                <span className="sr-only">open menu</span>
-                <Icons.MoreHorizontal />
-              </Button>
-            </DropdownMenuTrigger>
-          </div>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem asChild>
-              <Link
-                href={AppRoutes.editInterview(interview.id)}
-                className="gap-2"
-              >
-                <Icons.Pencil className="w-4 lg:w-5" />
-                edit
-              </Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
-
 type InterviewsTableProps = {};
 
 const InterviewsTable = (props: InterviewsTableProps) => {
+  // hooks
+  const queryClient = useQueryClient();
   const { data = [], isLoading } = useInterviews();
-  const columns = useMemo(() => tableColumns, []);
+
+  // functions
+  const handleDeleteInterview = useCallback(
+    (id: InterviewType["id"]) => {
+      appApi
+        .delete(ApiRoutes.deleteInterview(id))
+        .then(() => {
+          toast.success("interview deleted");
+          queryClient.invalidateQueries({
+            queryKey: [QueryKeys.INTERVIEWS],
+          });
+        })
+        .catch(() => {
+          toast.error("failed to delete interview, please try again");
+        });
+    },
+    [queryClient],
+  );
+
+  // constants
+  const columns = useMemo<ColumnDef<InterviewType>[]>(
+    () => [
+      {
+        accessorKey: "status",
+        header: "status",
+        cell: ({ row }) => {
+          const status = row.original.status;
+          return (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  {interviewStatusOptionsMap[status].icon}
+                </TooltipTrigger>
+                <TooltipContent color="neutral">
+                  {interviewStatusOptionsMap[status].label}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          );
+        },
+      },
+      {
+        accessorKey: "company_name",
+        header: "company name",
+      },
+      {
+        accessorKey: "position",
+        header: "position",
+        enableSorting: false,
+      },
+      {
+        accessorKey: "rounds",
+        header: "rounds",
+        cell: ({ row }) => {
+          const rounds = row.original.rounds;
+          return rounds.length;
+        },
+      },
+      {
+        accessorKey: "start_date",
+        header: "start_date",
+        cell: ({ row }) => {
+          return dayjs(row.original.start_date).format("DD/MM/YYYY");
+        },
+        sortingFn: (rowA, rowB) => {
+          const startDateA = rowA.original.start_date;
+          const startDateB = rowB.original.start_date;
+
+          if (!dayjs(startDateA).isValid() || !dayjs(startDateB).isValid()) {
+            return 0;
+          }
+
+          return dayjs(startDateA).diff(dayjs(startDateB));
+        },
+      },
+      {
+        accessorKey: "end_date",
+        header: "end date",
+        cell: ({ row }) => {
+          const endDate: string = row.getValue("end_date");
+
+          if (!endDate || !dayjs(endDate).isValid()) {
+            return "-";
+          }
+
+          return dayjs(row.getValue("end_date")).format("DD/MM/YYYY");
+        },
+      },
+      {
+        accessorKey: "result",
+        header: "result",
+        cell: ({ row }) => {
+          const result = row.original.result;
+
+          if (!result) {
+            return "-";
+          }
+
+          const option = interviewResultOptionsMap[result];
+          return (
+            <Flex className="gap-3xs" align="center">
+              {option.icon} {option.label}
+            </Flex>
+          );
+        },
+        enableSorting: false,
+      },
+      {
+        id: "actions",
+        cell: ({ row }) => {
+          const interview = row.original;
+
+          return (
+            <DropdownMenu>
+              <div className="w-full text-right">
+                <DropdownMenuTrigger asChild>
+                  <Button color="ghost" responsive>
+                    <span className="sr-only">open menu</span>
+                    <Icons.MoreHorizontal />
+                  </Button>
+                </DropdownMenuTrigger>
+              </div>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem asChild>
+                  <Link
+                    href={AppRoutes.editInterview(interview.id)}
+                    className="gap-2"
+                  >
+                    <Icons.Pencil className="w-4 lg:w-5" />
+                    edit
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="gap-2 text-error"
+                  onClick={() => handleDeleteInterview(interview.id)}
+                >
+                  <Icons.Trash2 className="w-4 lg:w-5" />
+                  delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          );
+        },
+      },
+    ],
+    [handleDeleteInterview],
+  );
 
   const [sorting, setSorting] = useState<SortingState>([]);
 
@@ -162,17 +213,8 @@ const InterviewsTable = (props: InterviewsTableProps) => {
     getCoreRowModel: getCoreRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
-    // getFilteredRowModel: getFilteredRowModel(),
-    // onColumnFiltersChange: setColumnFilters,
-    // getPaginationRowModel: getPaginationRowModel(),
-    // onColumnVisibilityChange: setColumnVisibility,
-    // onRowSelectionChange: setRowSelection,
     state: {
       sorting,
-
-      // columnFilters,
-      // columnVisibility,
-      // rowSelection,
     },
   });
 
