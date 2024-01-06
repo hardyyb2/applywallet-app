ac7f19440780c1ab87efe553f9a4c682bc785262
hardik badola <hardikb3732@gmail.com>
2024-01-06 10:19:36 +0530

    [UPDATED] Sheet component styles and add new components

--- common/components/ds/Sheet/Sheet.tsx
+++ common/components/ds/Sheet/Sheet.tsx
@@ -45,13 +45,13 @@ const sheetVariants = cva(
         top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
         bottom:
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
-        left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
+        left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left md:max-w-lg",
         right:
-          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right md:max-w-lg",
       },
     },
     defaultVariants: {
-      side: "left",
+      side: "right",
     },
   },
 );
@@ -65,26 +65,83 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "left", className, children, showClose = true, ...props }, ref) => (
-  <SheetPrimitive.Content
-    ref={ref}
-    className={cnM(sheetVariants({ side }), className)}
-    {...props}
-  >
-    {children}
-    {showClose ? (
-      <SheetPrimitive.Close
-        asChild
-        className="absolute right-4 top-4 opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none"
-      >
-        <Button color="ghost" shape="square" size="sm">
-          <Icons.X />
-        </Button>
-      </SheetPrimitive.Close>
-    ) : null}
-  </SheetPrimitive.Content>
-));
+>(
+  (
+    { side = "right", className, children, showClose = true, ...props },
+    ref,
+  ) => (
+    <SheetPrimitive.Content
+      ref={ref}
+      className={cnM(sheetVariants({ side }), className)}
+      {...props}
+    >
+      {children}
+      {showClose ? (
+        <SheetPrimitive.Close
+          asChild
+          className="absolute right-4 top-4 opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none"
+        >
+          <Button color="ghost" shape="square" size="md">
+            <Icons.X />
+          </Button>
+        </SheetPrimitive.Close>
+      ) : null}
+    </SheetPrimitive.Content>
+  ),
+);
 SheetContent.displayName = SheetPrimitive.Content.displayName;
+
+const SheetHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cnM(
+      "flex flex-col gap-3xs p-s text-center sm:text-left",
+      className,
+    )}
+    {...props}
+  />
+);
+SheetHeader.displayName = "SheetHeader";
+
+const SheetFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cnM(
+      "absolute inset-x-0 bottom-0 flex bg-base-100 p-xs",
+      className,
+    )}
+    {...props}
+  />
+);
+SheetFooter.displayName = "SheetFooter";
+
+const SheetTitle = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <SheetPrimitive.Title
+    ref={ref}
+    className={cnM("title-m lg:title-l", className)}
+    {...props}
+  />
+));
+SheetTitle.displayName = SheetPrimitive.Title.displayName;
+
+const SheetDescription = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Description>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <SheetPrimitive.Description
+    ref={ref}
+    className={cnM("label-xs lg:label-m", className)}
+    {...props}
+  />
+));
+SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
 export {
   Sheet,
@@ -93,4 +150,8 @@ export {
   SheetTrigger,
   SheetClose,
   SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
 };
