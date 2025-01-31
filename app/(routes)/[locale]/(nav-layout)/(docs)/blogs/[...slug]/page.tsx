0c625ab6e927c3ef6946167f84dff8652d96e0b1
hardik badola <hardikb3732@gmail.com>
2025-01-31 19:38:13 +0530

    [ADDED] breadcrumbs to the blog page

--- app/(routes)/[locale]/(nav-layout)/(docs)/blogs/[...slug]/page.tsx
+++ app/(routes)/[locale]/(nav-layout)/(docs)/blogs/[...slug]/page.tsx
@@ -3,8 +3,16 @@ import { notFound } from "next/navigation";
 
 import { allBlogs } from "contentlayer/generated";
 
+import { Breadcrumbs } from "~/components/ds/Breadcrumbs";
 import { Flex } from "~/components/ds/Flex";
+import {
+  ScrollArea,
+  ScrollAreaViewport,
+  ScrollBar,
+} from "~/components/ds/ScrollArea";
+import { AppRoutes } from "~/utils/routes";
 
+import { IconLink } from "@/components/dependent/IconLink";
 import { Mdx } from "@/components/dependent/Mdx";
 import { shimmer } from "@/utils/shimmer";
 import { toBase64 } from "@/utils/string";
@@ -53,6 +61,17 @@ const BlogPage = async (props: BlogPageProps) => {
   return (
     <Flex direction="column" className="h-full">
       <BlogScrollWrapper>
+        <ScrollArea className="mb-2xs-xs w-full lg:block">
+          <ScrollAreaViewport>
+            <Breadcrumbs className="mb-2xs flex-0 pt-0 [&_a]:no-underline">
+              <Breadcrumbs.Item>
+                <IconLink href={AppRoutes.BLOGS} />
+              </Breadcrumbs.Item>
+              <Breadcrumbs.Item>{blog.title}</Breadcrumbs.Item>
+            </Breadcrumbs>
+          </ScrollAreaViewport>
+          <ScrollBar orientation="horizontal" className="hidden h-0" />
+        </ScrollArea>
         <Mdx className="prose prose-sm dui-prose w-full md:prose-base lg:prose-lg xl:prose-xl">
           <Mdx.Header>
             <h1>{blog.title}</h1>
