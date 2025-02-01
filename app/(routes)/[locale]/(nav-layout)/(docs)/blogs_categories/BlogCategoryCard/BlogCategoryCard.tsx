44e6192e2857915d29a3e8483fe584a73ca429c0
hardik badola <hardikb3732@gmail.com>
2025-02-01 12:13:05 +0530

    [UPDATED] Integrate DOMPurify for sanitizing blog category images

--- app/(routes)/[locale]/(nav-layout)/(docs)/blogs_categories/BlogCategoryCard/BlogCategoryCard.tsx
+++ app/(routes)/[locale]/(nav-layout)/(docs)/blogs_categories/BlogCategoryCard/BlogCategoryCard.tsx
@@ -1,4 +1,5 @@
 import type { BlogCategory } from "contentlayer/generated";
+import DOMPurify from "isomorphic-dompurify";
 
 import { Card } from "~/components/ds/Card";
 
@@ -11,21 +12,15 @@ type BlogCategoryCardProps = {
 const BlogCategoryCard = ({ category }: BlogCategoryCardProps) => {
   const { image, name } = category;
 
-  console.log("image" + image);
+  const imageHtml = image ? DOMPurify.sanitize(image) : null;
 
   return (
-    <Card
-      className={cn(
-        "group relative w-full",
-        "before:absolute before:-left-2 before:-z-[1] before:h-full before:w-full before:rounded-2xl before:bg-accent",
-      )}
-      compact
-    >
+    <Card className={cn("group relative w-full")} compact>
       <Card.Body className="items-center bg-base-200">
-        {image ? (
+        {imageHtml ? (
           <Card.Figure
-            className={cn("m-3xs h-16 w-2xl min-w-2xl rounded-xl")}
-            dangerouslySetInnerHTML={{ __html: image }}
+            className={cn("m-3xs h-xl w-auto")}
+            dangerouslySetInnerHTML={{ __html: imageHtml }}
           />
         ) : null}
         <Card.Title className="title-m line-clamp-1">{name}</Card.Title>
