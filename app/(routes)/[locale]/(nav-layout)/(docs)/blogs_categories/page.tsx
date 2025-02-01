9af94b97f5d0f22b0d92bbc72db77bbe165c2a2e
hardik badola <hardikb3732@gmail.com>
2025-02-01 12:44:49 +0530

    [UPDATED] Refactor blog categories layout and integrate category cards

--- app/(routes)/[locale]/(nav-layout)/(docs)/blogs_categories/page.tsx
+++ app/(routes)/[locale]/(nav-layout)/(docs)/blogs_categories/page.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Link from "next/link";
+
+import { allBlogCategories } from "contentlayer/generated";
+
+import { Flex } from "~/components/ds/Flex";
+
+import { BlogCategoryCard } from "./BlogCategoryCard/BlogCategoryCard";
+import { BlogCategoryCardsWrapper } from "./BlogCategoryCardsWrapper";
 
 const BlogsCategoriesPage = () => {
-  return <div>BlogsCategoriesPage</div>;
+  return (
+    <div className="px-5 py-4 lg:px-10">
+      <Flex direction="column" className="mb-s-m">
+        <h1 className="headline-s lg:headline-m">blog categories</h1>
+        <h4 className="label-s lg:label-m">categories for blogs</h4>
+      </Flex>
+      <BlogCategoryCardsWrapper>
+        {allBlogCategories.map((category) => {
+          return (
+            <Link
+              href={category.slug}
+              key={category._id}
+              className="w-full rounded-2xl"
+            >
+              <BlogCategoryCard category={category} />
+            </Link>
+          );
+        })}
+      </BlogCategoryCardsWrapper>
+    </div>
+  );
 };
 
 export default BlogsCategoriesPage;
