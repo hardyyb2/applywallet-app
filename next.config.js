e4e48f7d5fbf4e2d603d61be8e8f7521954e07d7
hardik badola <hardikb3732@gmail.com>
2025-02-01 12:00:03 +0530

    [UPDATED] change hostname for image source in next.config.js

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "picsum.photos",
+        hostname: "fastly.picsum.photos",
       },
     ],
     // TODO - remove picsum
