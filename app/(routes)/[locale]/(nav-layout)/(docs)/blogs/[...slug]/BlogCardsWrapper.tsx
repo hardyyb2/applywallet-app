2e19321629b3b347f4c39b52ddc3a77413b0b5c2
hardik badola <hardikb3732@gmail.com>
2025-02-02 18:37:52 +0530

    [REMOVED] BlogCardsWrapper component for improved layout; remove unused dependencies masonry

--- app/(routes)/[locale]/(nav-layout)/(docs)/blogs/[...slug]/BlogCardsWrapper.tsx
+++ app/(routes)/[locale]/(nav-layout)/(docs)/blogs/[...slug]/BlogCardsWrapper.tsx
@@ -1,40 +1,10 @@
-"use client";
-
 import { type PropsWithChildren } from "react";
-import dynamic from "next/dynamic";
-
-import { BarLoader } from "~/components/ds/BarLoader";
-
-import { useBreakPoint } from "@/hooks/useBreakPoint";
-
-const ResponsiveMasonry = dynamic(
-  () => import("react-responsive-masonry").then((mod) => mod.ResponsiveMasonry),
-  {
-    loading: () => <BarLoader />,
-    ssr: false,
-  },
-);
-const Masonry = dynamic(
-  () => import("react-responsive-masonry").then((mod) => mod.default),
-  {
-    loading: () => <BarLoader />,
-    ssr: false,
-  },
-);
 
 const BlogCardsWrapper = ({ children }: PropsWithChildren) => {
-  const { isBelowLg } = useBreakPoint("lg");
-
-  if (isBelowLg) {
-    return <div className="grid grid-cols-1 gap-xs">{children}</div>;
-  }
-
   return (
-    <ResponsiveMasonry
-      columnsCountBreakPoints={{ 300: 1, 768: 2, 1440: 3, 1920: 4 }}
-    >
-      <Masonry gutter="1rem">{children}</Masonry>
-    </ResponsiveMasonry>
+    <div className="grid grid-cols-1 gap-xs-s lg:grid-cols-[repeat(auto-fill,minmax(360px,1fr))]">
+      {children}
+    </div>
   );
 };
 
