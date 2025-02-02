b2263aa6e92f4e4548836b30c7af1af361e668b8
hardik badola <hardikb3732@gmail.com>
2025-02-02 11:20:53 +0530

    [ADDED] experimental Turbo configuration for SVG handling with SVGR

--- next.config.js
+++ next.config.js
@@ -27,6 +27,16 @@ const nextConfig = {
     });
     return config;
   },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 const contentLayerWrapper = withContentlayer(nextConfig);
