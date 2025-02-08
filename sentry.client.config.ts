968b2209d7ff52e8000cb321b5b4c911c8a258b5
hardik badola <hardikb3732@gmail.com>
2025-02-08 13:16:34 +0530

    [UPDATED] Development scripts and Sentry environment configuration

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,5 +6,5 @@ Sentry.init({
   dsn: "https://3f61abf4d77bb0cfbe6a2aef0a8cae07@o4506184356003840.ingest.us.sentry.io/4506184358035456",
   tracesSampleRate: 1,
   debug: false,
-  enabled: env.NODE_ENV === "production",
+  enabled: env.NEXT_PUBLIC_NODE_ENV === "production",
 });
