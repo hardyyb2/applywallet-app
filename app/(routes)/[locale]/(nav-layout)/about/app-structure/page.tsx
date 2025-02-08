8b91734dd041e0bdea1a2af504c24a59a93c76d9
hardik badola <hardikb3732@gmail.com>
2025-02-08 14:19:12 +0530

    [UPDATED] App structure page text and heading

--- app/(routes)/[locale]/(nav-layout)/about/app-structure/page.tsx
+++ app/(routes)/[locale]/(nav-layout)/about/app-structure/page.tsx
@@ -8,9 +8,9 @@ const ProjectStructure = () => {
     <Flex direction="column" className="h-full w-full px-xs py-4 lg:px-s">
       <Flex justify="space-between" className="mb-xs flex-0 gap-4">
         <Flex direction="column">
-          <h1 className="headline-s lg:headline-m">project structure</h1>
+          <h1 className="headline-s lg:headline-m">app structure</h1>
           <h4 className="label-s lg:label-m">
-            see the entire app directory structure
+            see the entire directory structure
           </h4>
         </Flex>
       </Flex>
