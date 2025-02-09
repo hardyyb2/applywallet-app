c71de01365b345053b9b5261ab1e4d2d527f0a9f
hardik badola <hardikb3732@gmail.com>
2025-02-09 11:13:46 +0530

    [ADDED] AboutApp component for About page with app status and blog link

---
+++ app/(routes)/[locale]/(nav-layout)/about/AboutApp.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { buttonVariants } from "~/components/ds/Button";
+import { Card } from "~/components/ds/Card";
+import { Typography } from "~/components/ds/Typography";
+import { AppRoutes } from "~/utils/routes";
+
+import AwLogoImg from "public/images/logo/aw-logo.png";
+
+const AboutApp = () => {
+  return (
+    <Card className="overflow-hidden">
+      <Card.Body className="items-center gap-xs">
+        <Card.Figure className="mt-xs h-32 w-32 lg:h-40 lg:w-40">
+          <Image
+            src={AwLogoImg}
+            alt="applywallet"
+            width={120}
+            height={120}
+            className="z-[1] h-full w-full object-cover"
+          />
+        </Card.Figure>
+
+        <Card.Title className="z-[1] text-base-content">
+          <Typography variant="headline-s">applywallet</Typography>
+        </Card.Title>
+        <Typography
+          variant="body-m"
+          align="center"
+          className="z-[1] text-base-content"
+        >
+          is under construction, till then check out our blogs
+        </Typography>
+
+        <Card.Actions className="z-[1] w-full">
+          <Link
+            href={AppRoutes.BLOGS}
+            className={buttonVariants({
+              color: "primary",
+              fullWidth: true,
+            })}
+          >
+            blogs
+          </Link>
+        </Card.Actions>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export { AboutApp };
--- app/(routes)/[locale]/(nav-layout)/about/page.tsx
+++ app/(routes)/[locale]/(nav-layout)/about/page.tsx
@@ -1,14 +1,16 @@
 import { BasePageHeader } from "@/components/dependent/BasePageHeader";
 
+import { AboutApp } from "./AboutApp";
 import { AboutCreator } from "./AboutCreator";
 
 const AboutPage = () => {
   return (
     <div className="h-full px-5 py-4 lg:px-10">
-      <BasePageHeader title="about" subtitle="" />
+      <BasePageHeader title="about" subtitle="app & creator" />
 
       <div className="grid grid-cols-1 gap-xs-s lg:grid-cols-2">
-        <div>About App</div>
+        <AboutApp />
+
         <AboutCreator />
       </div>
     </div>
