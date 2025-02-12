d4ee8abb76b09ec42e3cca871959e8ab0edf3412
hardik badola <hardikb3732@gmail.com>
2025-02-12 21:50:43 +0530

    [UPDATED] Breakpoints configuration and media query handling

--- app/_hooks/useBreakPoint.ts
+++ app/_hooks/useBreakPoint.ts
@@ -6,7 +6,7 @@ import { breakpoints, type BreakPointKeyType } from "~/utils/tailwind/theme";
 
 const useBreakPoint = <K extends BreakPointKeyType>(breakPointKey: K) => {
   const breakPointValue = breakpoints[breakPointKey];
-  const bool = useMedia(`(max-width: ${breakPointValue})`, false);
+  const bool = useMedia(`(max-width: ${breakPointValue}px)`, false);
 
   const capitalizedKey =
     breakPointKey[0].toUpperCase() + breakPointKey.substring(1);
--- common/utils/tailwind/theme.ts
+++ common/utils/tailwind/theme.ts
@@ -1,7 +1,3 @@
-import tailwindConfig from "tailwind.config";
-
-const fullConfig = {};
-
 /* Find active themes in `global.app.css` */
 const appThemes: string[] = ["light", "synthwave"];
 
@@ -57,11 +53,20 @@ function extractThemeColorsFromDOM(): ThemeColors {
   };
 }
 
-const breakpoints = {};
+/** Find active breakpoints in `global.app.css` */
+const breakpoints = {
+  xs: 480,
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  "2xl": 1440,
+  "3xl": 1920,
+  "4xl": 2560,
+} as const;
 type BreakPointKeyType = keyof typeof breakpoints;
 
 export {
-  fullConfig as tailwindConfig,
   appThemes,
   breakpoints,
   extractThemeColorsFromDOM,
