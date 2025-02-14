3480c96fd856f2a0d4c15008cdb1b4da54360c2d
hardik badola <hardikb3732@gmail.com>
2025-02-14 22:09:35 +0530

    [UPDATED] Animation and slide classes for Dialog and Sheet components

--- common/components/ds/Sheet/Sheet.tsx
+++ common/components/ds/Sheet/Sheet.tsx
@@ -43,12 +43,12 @@ const sheetVariants = cva(
   {
     variants: {
       side: {
-        top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
+        top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top-100 data-[state=open]:slide-in-from-top-100",
         bottom:
-          "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
-        left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left md:max-w-(--breakpoint-lg)",
+          "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom-100 data-[state=open]:slide-in-from-bottom-100",
+        left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left-100 data-[state=open]:slide-in-from-left-100 md:max-w-lg",
         right:
-          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right md:max-w-(--breakpoint-lg)",
+          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right-100 data-[state=open]:slide-in-from-right-100 md:max-w-lg",
       },
     },
     defaultVariants: {
