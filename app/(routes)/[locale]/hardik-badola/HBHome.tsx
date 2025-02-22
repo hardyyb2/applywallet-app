e88fcd5ab6d6b8569d39d1b8db91a19445d6d3c7
hardik badola <hardikb3732@gmail.com>
2025-02-22 12:36:10 +0530

    [ADDED] Hardik Badola Home Page with Split-Screen Layout
    - Created HBHome component with responsive typography
    - Implemented split-screen design using clip-path
    - Added main content section with personal introduction
    - Utilized Flex and Typography components for layout and styling

---
+++ app/(routes)/[locale]/hardik-badola/HBHome.tsx
@@ -0,0 +1,44 @@
+import { Flex } from "~/components/ds/Flex";
+import { Typography } from "~/components/ds/Typography";
+
+import { cnM } from "@/utils/styles";
+
+const MainContent = () => {
+  return (
+    <Flex
+      align="center"
+      direction="column"
+      justify="center"
+      className="gap-3xs h-full w-full"
+    >
+      <Typography variant="headline-l" lg="display-l" align="center">
+        hello, I&apos;m hardik badola
+      </Typography>
+      <Typography variant="label-l" md="title-l" lg="headline-l" font="primary">
+        a software engineer
+      </Typography>
+    </Flex>
+  );
+};
+
+const HBHome = () => {
+  return (
+    <main className={cnM("relative h-full w-full overflow-hidden")}>
+      <div
+        className="absolute top-0 left-0 flex h-screen w-screen"
+        style={{ clipPath: `inset(0 50% 0 0)` }}
+      >
+        <MainContent />
+      </div>
+
+      <div
+        className="bg-primary text-primary-content absolute top-0 left-0 flex h-screen w-screen"
+        style={{ clipPath: `inset(0 0 0 50%)` }}
+      >
+        <MainContent />
+      </div>
+    </main>
+  );
+};
+
+export { HBHome };
