dbe25c10f36b55e0175e62be1a89d89775f98a5e
hardik badola <hardikb3732@gmail.com>
2025-02-23 16:25:39 +0530

    [UPDATED] HB Navbar Logo - SVG Optimization and Rendering
    - Replaced raster logo image with optimized SVG
    - Updated logo rendering to use SVG component with fill-current class
    - Reduced logo size from 12 to 10 units for better visual balance
    - Removed commented-out Image component

--- app/(routes)/[locale]/hardik-badola/nav/HBNavbar.tsx
+++ app/(routes)/[locale]/hardik-badola/nav/HBNavbar.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import { NavBar } from "~/components/ds/NavBar";
 import { AppRoutes } from "~/utils/routes";
 
-import HardikBadolaImg from "public/images/logo/hb-logo.jpeg";
+import HardikBadolaImg from "public/images/logo/HB.svg";
 
 import { LocaleSwitcher } from "@/components/dependent/LocaleSwitcher";
 import { ThemesMenu } from "@/components/dependent/ThemesMenu";
@@ -29,15 +28,8 @@ const HBNavbar = () => {
     <NavBar className="pl-xs md:pl-s lg:pl-l bg-base-100">
       <NavBar.Start className="gap-m pr-m flex">
         <Link href={AppRoutes.CREATOR}>
-          <figure className="relative h-12 w-12">
-            <Image
-              src={HardikBadolaImg}
-              alt="Hardik Badola"
-              fill
-              placeholder="blur"
-              blurDataURL={HardikBadolaImg.blurDataURL}
-              className="h-full w-full rounded-full"
-            />
+          <figure className="relative h-10 w-10">
+            <HardikBadolaImg className="h-full w-full fill-current" />
           </figure>
         </Link>
       </NavBar.Start>
